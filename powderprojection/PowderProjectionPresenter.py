1ab8a0beddd67e83954a307adad9cc716ac3ee11
Eltayeb Ahmed <eltayeb.ahmed@stfc.ac.uk>
2016-07-20 15:20:18 +0100

    Started on projection calculator

--- powderprojection/PowderProjectionPresenter.py
+++ powderprojection/PowderProjectionPresenter.py
@@ -3,17 +3,18 @@ from command import Command
 
 class PowderProjectionPresenter(object):
 
-    def __init__(self,mainView,powderView,projection_calculator):
+    def __init__(self,powderView,mainView,projection_calculator):
         self._powderProjectionView = powderView
         self._projection_calculator = projection_calculator
-        self._workspace_manager_presenter = mainView.get_workspace_manager_presenter()
+        #self._workspace_manager_presenter = mainView.get_workspace_manager_presenter()
         #Add reset of options
         self._powderProjectionView.populate_powder_u1(['Energy'])
         self._powderProjectionView.populate_powder_u2(['|Q|'])
 
     def notify(self,command):
         if command == Command.CalculatePowderProjection:
+            self._powderProjectionView.display_start_projecting('projecting')
             axis1 = self._powderProjectionView.get_powder_u1()
             axis2 = self._powderProjectionView.get_powder_u2()
-            a
+
 
--- powderprojection/PowderProjectionQuickView.py
+++ powderprojection/PowderProjectionQuickView.py
@@ -4,5 +4,5 @@ from powderprojection.PowderProjectionPresenter import PowderProjectionPresenter
 class PowderProjectionQuickView(QuickView):
     def __init__(self,commands):
         super(PowderProjectionQuickView,self).__init__(commands)
-        self._presenter = PowderProjectionPresenter(self)
+        self._presenter = PowderProjectionPresenter(self,None,None)
 
