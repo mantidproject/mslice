5b6070be096de49eee544191391377902d1e6e50
Eltayeb Ahmed <eltayeb.ahmed@stfc.ac.uk>
2016-08-11 17:01:13 +0100

    Added docstrings

--- build_all_ui_files.py
+++ build_all_ui_files.py
@@ -1,11 +1,11 @@
 from PyQt4 import uic
 import os
-script_path = os.path.realpath(__file__)
+script_path = os.path.realpath(__file__) # assumes script is in project root directory
 script_folder = os.path.dirname(script_path)
 
 
 def remove_extension(filename):
-    """return a the filename without the extension"""
+    """return the filename without the extension"""
     dot_occurrences = [n for n in xrange(len(filename)) if filename.find('.', n) == n]
     return filename[:dot_occurrences[-1]]
 
@@ -14,17 +14,20 @@ def is_ui_file(item):
     return item.endswith('.ui')
 
 
-def get_output_file_name_item(filename):
+def get_output_file_name(filename):
+    """Naming scheme foo.ui ==> foo_ui.py"""
     output_file_name = os.path.basename(filename)
     output_file_name = remove_extension(output_file_name)
     output_file_name += '_ui.py'
     return output_file_name
 
 
-def build_item(item,verbose=True):
+def build_item(item, verbose=True):
+    """Build the ui file at the path in "item" to python file in the same directory.
+     The output file name is specified by the get_output_file_name function"""
     dir = os.path.dirname(item)
     basename = os.path.basename(item)
-    output_file_name = get_output_file_name_item(basename)
+    output_file_name = get_output_file_name(basename)
     output_file_path = os.path.join(dir,output_file_name)
     with open(output_file_path, 'w') as fout:
         uic.compileUi(item, fout)
@@ -34,6 +37,7 @@ def build_item(item,verbose=True):
 
 
 def build_all_ui_files(basepath):
+    """Build all ui files found in folder basepath"""
     for item in os.listdir(basepath):
         if os.path.isdir(os.path.join(basepath,item)):
             build_all_ui_files(os.path.join(basepath,item))
@@ -42,4 +46,5 @@ def build_all_ui_files(basepath):
                 build_item(os.path.join(basepath,item))
 
 if __name__ == '__main__':
+    # assumes script is in project root directory
     build_all_ui_files(script_folder)
