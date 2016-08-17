2250bb8f2b7f8adaa1b8f85b051f8854af09f187
Eltayeb Ahmed <eltayeb.ahmed@stfc.ac.uk>
2016-08-17 10:31:50 +0100

    extract constant from function call to variable

--- models/projection/powder/mantid_projection_calculator.py
+++ models/projection/powder/mantid_projection_calculator.py
@@ -1,11 +1,13 @@
 from mantid.simpleapi import ConvertToMD
 from models.projection.powder.projection_calculator import ProjectionCalculator
 
+MD_SUFFIX = '_MD'
+
 
 class MantidProjectionCalculator(ProjectionCalculator):
     def calculate_projection(self, input_workspace, output_workspace, qbinning, axis1, axis2):
         if axis1 == '|Q|' and axis2 == 'Energy':
-            ConvertToMD(InputWorkspace=input_workspace, OutputWorkspace=input_workspace+'_MD',QDimensions='|Q|',
+            ConvertToMD(InputWorkspace=input_workspace, OutputWorkspace=input_workspace + MD_SUFFIX, QDimensions='|Q|',
                         PreprocDetectorsWS='-')
         else:
             raise NotImplementedError('MSlice currently only supports projection to Energy vs |Q|')
