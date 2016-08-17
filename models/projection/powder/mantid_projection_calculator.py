4f7e8cb10ca3eda75c4347d5ed4ee80a3a5f295a
Eltayeb Ahmed <eltayeb.ahmed@stfc.ac.uk>
2016-08-17 10:15:55 +0100

    Disable caching of detector workspaces in ConvertToMD

--- models/projection/powder/mantid_projection_calculator.py
+++ models/projection/powder/mantid_projection_calculator.py
@@ -5,7 +5,8 @@ from models.projection.powder.projection_calculator import ProjectionCalculator
 class MantidProjectionCalculator(ProjectionCalculator):
     def calculate_projection(self, input_workspace, output_workspace, qbinning, axis1, axis2):
         if axis1 == '|Q|' and axis2 == 'Energy':
-            ConvertToMD(InputWorkspace=input_workspace, OutputWorkspace=input_workspace+'_MD',QDimensions='|Q|')
+            ConvertToMD(InputWorkspace=input_workspace, OutputWorkspace=input_workspace+'_MD',QDimensions='|Q|',
+                        PreprocDetectorsWS='-')
         else:
             raise NotImplementedError('MSlice currently only supports projection to Energy vs |Q|')
 
