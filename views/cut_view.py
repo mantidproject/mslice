e85701d3dff35a2eb3ceb48368fb27063eeb4bf9
Eltayeb Ahmed <eltayeb.ahmed@stfc.ac.uk>
2016-08-18 15:45:16 +0100

    Implemented the view for the cut widget

---
+++ views/cut_view.py
@@ -0,0 +1,38 @@
+
+
+class CutView:
+    def get_cut_axis(self):
+        pass
+
+    def get_cut_axis_start(self):
+        pass
+
+    def get_cut_axis_end(self):
+        pass
+
+    def get_cut_axis_step(self):
+        pass
+
+    def get_integration_axis_start(self):
+        pass
+
+    def get_integration_axis_end(self):
+        pass
+
+    def get_integration_width(self):
+        pass
+
+    def get_intensity_start(self):
+        pass
+
+    def get_intensity_end(self):
+        pass
+
+    def get_intensity_is_norm_to_one(self):
+        pass
+
+    def get_smoothing(self):
+        pass
+
+    def populate_cut_axis_options(self,options):
+        pass
--- widgets/cut/cut.py
+++ widgets/cut/cut.py
@@ -1,8 +1,46 @@
 from cut_ui import Ui_Form
+from views.cut_view import CutView
 from PyQt4.QtGui import QWidget
 
 
-class CutWidget(QWidget,Ui_Form):
+class CutWidget(QWidget, CutView, Ui_Form):
     def __init__(self,*args, **kwargs):
         super(CutWidget,self).__init__(*args, **kwargs)
         self.setupUi(self)
+
+
+    def set_workspace_selector(self, workspace_selector):
+        pass
+
+    def get_cut_axis(self):
+        return str(self.cmbCutAxis.currentText())
+
+    def get_cut_axis_start(self):
+        return str(self.lneCutStart.text())
+
+    def get_cut_axis_step(self):
+        return str(self.lneCutStep.text())
+
+    def get_cut_axis_end(self):
+        return str(self.lneCutEnd.text())
+
+    def get_integration_axis_start(self):
+        return str(self.lneCutIntegrationStart.text())
+
+    def get_integration_axis_end(self):
+        return str(self.lneCutIntegrationEnd.text())
+
+    def get_integration_width(self):
+        return str(self.lneCutIntegrationEnd.text())
+
+    def get_intensity_start(self):
+        return str(self.lneEditCutIntensityStart.text())
+
+    def get_intensity_end(self):
+        return str(self.lneCutIntensityEnd.text())
+
+    def get_intensity_is_norm_to_one(self):
+        return self.rdoCutNormToOne.isChecked()
+
+    def get_smoothing(self):
+        return str(self.lneCutSmoothing.text())
