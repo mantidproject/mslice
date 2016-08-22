53850319b38efeea6ce4b94635252702a1da4a54
Eltayeb Ahmed <eltayeb.ahmed@stfc.ac.uk>
2016-08-22 10:37:34 +0100

    raise NotImplemented in abstract view methods

--- views/workspace_view.py
+++ views/workspace_view.py
@@ -1,35 +1,38 @@
 
 
 class WorkspaceView(object):
+    def __init__(self):
+        raise Exception("This abstact base class must not be instantiated")
+    
     def display_loaded_workspaces(self, workspaces):
-        pass
+        raise NotImplementedError("This method must be implemented in a concrete view before being called")
 
     def get_workspace_to_load_path(self):
-        pass
+        raise NotImplementedError("This method must be implemented in a concrete view before being called")
 
     def get_workspace_to_save_filepath(self):
-        pass
+        raise NotImplementedError("This method must be implemented in a concrete view before being called")
 
     def get_workspace_new_name(self):
-        pass
+        raise NotImplementedError("This method must be implemented in a concrete view before being called")
 
     def get_workspace_selected(self):
-        pass
+        raise NotImplementedError("This method must be implemented in a concrete view before being called")
 
     def error_select_only_one_workspace(self):
-        pass
+        raise NotImplementedError("This method must be implemented in a concrete view before being called")
 
     def error_select_one_workspace(self):
-        pass
+        raise NotImplementedError("This method must be implemented in a concrete view before being called")
 
     def error_select_one_or_more_workspaces(self):
-        pass
+        raise NotImplementedError("This method must be implemented in a concrete view before being called")
 
     def get_presenter(self):
-        pass
+        raise NotImplementedError("This method must be implemented in a concrete view before being called")
 
     def confirm_overwrite_workspace(self):
-        pass
+        raise NotImplementedError("This method must be implemented in a concrete view before being called")
 
     def no_workspace_has_been_loaded(self):
-        pass
+        raise NotImplementedError("This method must be implemented in a concrete view before being called")
