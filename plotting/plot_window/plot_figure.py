c8caf326938ac63979fc5842e53c84a7b84c8bc4
Eltayeb Ahmed <eltayeb.ahmed@stfc.ac.uk>
2016-08-24 11:50:57 +0100

    Implemented data cursor

--- plotting/plot_window/plot_figure.py
+++ plotting/plot_window/plot_figure.py
@@ -1,26 +1,40 @@
 from base_qt_plot_window import BaseQtPlotWindow
 from plotting.plot_window.plot_window_ui import Ui_MainWindow
 from matplotlib.backends.backend_qt5 import NavigationToolbar2QT
+from PyQt4.QtCore import Qt
 
 class PlotFigure(BaseQtPlotWindow, Ui_MainWindow):
     def __init__(self,number,manager):
         super(PlotFigure,self).__init__(number, manager)
-        self.menuKeep.aboutToShow.connect(self._report_as_kept_to_manager)
-        self.menuMakeCurrent.aboutToShow.connect(self._report_as_current_to_manager)
+
+        self.actionKeep.triggered.connect(self._report_as_kept_to_manager)
+        self.actionMakeCurrent.triggered.connect(self._report_as_current_to_manager)
+
         self.actionDump_To_Console.triggered.connect(self._dump_script_to_console)
+
+        self.actionDataCursor.toggled.connect(self.toggle_data_cursor)
         self.stock_toolbar = NavigationToolbar2QT(self.canvas, self)
         self.stock_toolbar.hide()
 
         self.actionZoom_In.triggered.connect(self.stock_toolbar.zoom)
         self.actionZoom_Out.triggered.connect(self.stock_toolbar.back)
         self.action_save_image.triggered.connect(self.stock_toolbar.save_figure)
+
         self.show() #is not a good idea in non interactive mode
 
 
+    def toggle_data_cursor(self):
+        if self.actionDataCursor.isChecked():
+            self.stock_toolbar.message.connect(self.statusbar.showMessage)
+            self.canvas.setCursor(Qt.CrossCursor)
+        else:
+            self.stock_toolbar.message.disconnect()
+            self.canvas.setCursor(Qt.ArrowCursor)
+
     def _display_status(self,status):
         if status == "kept":
-            self.menuKeep.setEnabled(False)
-            self.menuMakeCurrent.setEnabled(True)
+            self.actionKeep.setChecked(True)
+            self.actionMakeCurrent.setChecked(False)
         elif status == "current":
-            self.menuMakeCurrent.setEnabled(False)
-            self.menuKeep.setEnabled(True)
+            self.actionMakeCurrent.setChecked(True)
+            self.actionKeep.setChecked(False)
