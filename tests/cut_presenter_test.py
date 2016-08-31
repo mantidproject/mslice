10e258afebc2bfa910f860042f8cd4ea0f35ab67
Eltayeb Ahmed <eltayeb.ahmed@stfc.ac.uk>
2016-08-31 11:37:39 +0100

    Added unittest

--- tests/cut_presenter_test.py
+++ tests/cut_presenter_test.py
@@ -5,6 +5,8 @@ from presenters.cut_presenter import CutPresenter
 from views.cut_view import CutView
 from models.cut.cut_algorithm import CutAlgorithm
 from presenters.interfaces.main_presenter import MainPresenterInterface
+from presenters.slice_plotter_presenter import Axis
+from widgets.cut.command import Command
 
 class CutPresenterTest(unittest.TestCase):
     def setUp(self):
@@ -51,16 +53,46 @@ class CutPresenterTest(unittest.TestCase):
         self.view.populate_cut_axis_options.assert_called_with(available_dimensions)
         self.view.enable.assert_called_with()
 
-    def test_workspace_selection_changed_single_cut_workspace(self):
+    # def test_workspace_selection_changed_single_cut_workspace(self):
+    #     cut_presenter = CutPresenter(self.view, self.cut_algorithm, self.plotting_module)
+    #     cut_presenter.register_master(self.main_presenter)
+    #     workspace = 'workspace'
+    #     self.main_presenter.get_selected_workspaces = mock.Mock(return_value=[workspace])
+    #     self.cut_algorithm.is_cuttable = mock.Mock(return_value=False)
+    #     self.cut_algorithm.is_cut = mock.Mock(return_value=True)
+    #     available_dimensions = ["dim1", "dim2"]
+    #     self.cut_algorithm.get_cut_params
+    #     cut_presenter.workspace_selection_changed()
+    #     self.view.populate_cut_axis_options.assert_called_with(available_dimensions)
+    #     self.view.enable.assert_called_with()
+
+    def test_plot_single_cut_success(self):
         cut_presenter = CutPresenter(self.view, self.cut_algorithm, self.plotting_module)
         cut_presenter.register_master(self.main_presenter)
-        workspace = 'workspace'
+        axis = Axis("units", "0", "100", "1")
+        processed_axis = Axis("units", 0, 100, 1)
+        integration_start = 3
+        integration_end = 5
+        width = ""
+        intensity_start = 11
+        intensity_end = 30
+        is_norm = True
+        workspace = "workspace"
+        integrated_axis = 'integrated axis'
         self.main_presenter.get_selected_workspaces = mock.Mock(return_value=[workspace])
-        self.cut_algorithm.is_cuttable = mock.Mock(return_value=False)
-        self.cut_algorithm.is_cut = mock.Mock(return_value=True)
-        available_dimensions = ["dim1", "dim2"]
-        self.cut_algorithm.get_available_axis = mock.Mock(return_value=available_dimensions)
-
-        cut_presenter.workspace_selection_changed()
-        self.view.populate_cut_axis_options.assert_called_with(available_dimensions)
-        self.view.enable.assert_called_with()
+        self.view.get_cut_axis = mock.Mock(return_value=axis.units)
+        self.view.get_cut_axis_start = mock.Mock(return_value=axis.start)
+        self.view.get_cut_axis_end = mock.Mock(return_value=axis.end)
+        self.view.get_cut_axis.step = mock.Mock(return_value=axis.step)
+        self.view.get_integration_start = mock.Mock(return_value=integration_start)
+        self.view.get_integration_end = mock.Mock(return_value=integration_end)
+        self.view.get_intensity_start = mock.Mock(return_value=intensity_start)
+        self.view.get_intensity_end = mock.Mock(return_value=intensity_end)
+        self.view.get_intensity_is_norm_to_one = mock.Mock(return_value=is_norm)
+        self.view.get_integration_width = mock.Mock(return_value=width)
+        self.cut_algorithm.compute_cut_xye = mock.Mock(return_value=('x', 'y', 'e'))
+        self.cut_algorithm.get_other_axis = mock.Mock(return_value=integrated_axis)
+        cut_presenter.notify(Command.Plot)
+        assert isinstance(self.cut_algorithm, CutAlgorithm)
+        self.cut_algorithm.compute_cut_xye.assert_called_with(workspace, processed_axis, integration_start,
+                                                              integration_end, is_norm)
