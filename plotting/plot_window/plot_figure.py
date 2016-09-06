22e18e532633cf03664b9ab03448f9d0e8e8d803
Eltayeb Ahmed <eltayeb.ahmed@stfc.ac.uk>
2016-09-06 11:47:56 +0100

    Implemented plot options dialog, can now change title and axis labels re #61

--- plotting/plot_window/plot_figure.py
+++ plotting/plot_window/plot_figure.py
@@ -44,10 +44,96 @@ class PlotFigureManager(BaseQtPlotWindow, Ui_MainWindow):
             self.actionKeep.setChecked(False)
 
     def _plot_options(self):
-        return
+        config = self._get_plot_description()
+        new_config = PlotOptionsDialog.get_new_config(config)
+        self._apply_config(new_config)
 
+    def _apply_config(self, plot_config):
+        current_axis = self.canvas.figure.gca()
+        current_axis.set_title(plot_config.title)
+        current_axis.set_xlabel(plot_config.xlabel)
+        current_axis.set_ylabel(plot_config.ylabel)
+        self.canvas.draw()
+
+    def _get_plot_description(self):
+        title = self.canvas.figure.gca().get_title()
+        xlabel = self.canvas.figure.gca().get_xlabel()
+        ylabel = self.canvas.figure.gca().get_ylabel()
+        return PlotConfig(title=title, x_axis_label=xlabel, y_axis_label=ylabel)
 
 class PlotOptionsDialog(QtGui.QDialog, Ui_Dialog):
-    def __init__(self):
+    def __init__(self, current_config):
         super(PlotOptionsDialog, self).__init__()
         self.setupUi(self)
+        self.groupBox.hide()
+        self.chkShowErrorBars.hide()
+        self.lneFigureTitle.setText(current_config.title)
+        self.lneXAxisLabel.setText(current_config.xlabel)
+        self.lneYAxisLabel.setText(current_config.ylabel)
+
+
+    @staticmethod
+    def get_new_config(current_config):
+        dialog = PlotOptionsDialog(current_config)
+        dialog.exec_()
+        return PlotConfig(title=dialog.lneFigureTitle.text(),
+                          x_axis_label=dialog.lneXAxisLabel.text(),
+                          y_axis_label=dialog.lneYAxisLabel.text())
+
+
+class PlotConfig(object):
+    def __init__(self, title=None, x_axis_label=None, y_axis_label=None, legends=None, errorbars_enabled=None):
+        self.title = title
+        self.xlabel = x_axis_label
+        self.ylabel = y_axis_label
+        self._legends = legends
+        self._errorbar = errorbars_enabled   # Has 3 values (True : shown, False: Not Shown, None: Not applicable)
+
+    @property
+    def title(self):
+        if self._title is not None:
+            return self._title
+        return ""
+
+    @title.setter
+    def title(self, value):
+        if value is None:
+            self._title = None
+        else:
+            try:
+                self._title = str(value)
+            except ValueError:
+                raise ValueError("Plot title must be a string or castable to string")
+
+    @property
+    def xlabel(self):
+        if self._xlabel is not None:
+            return self._xlabel
+        return ""
+
+    @xlabel.setter
+    def xlabel(self, value):
+        if value is None:
+            self._xlabel = None
+        else:
+            try:
+                self._xlabel = str(value)
+            except ValueError:
+                raise ValueError("Plot xlabel must be a string or castable to string")
+
+    @property
+    def ylabel(self):
+        if self._ylabel is not None:
+            return self._ylabel
+        return ""
+
+    @ylabel.setter
+    def ylabel(self, value):
+        if value is None:
+            self._ylabel = None
+        else:
+            try:
+                self._ylabel = str(value)
+            except ValueError:
+                raise ValueError("Plot ylabel must be a string or castable to string")
+
