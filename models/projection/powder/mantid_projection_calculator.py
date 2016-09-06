ded20a8ed90d4ae4f821cecdb58e93a2f851e465
Eltayeb Ahmed <eltayeb.ahmed@stfc.ac.uk>
2016-09-06 14:34:35 +0100

    Will transpose workspace if axis are switched in projections re #64

--- models/projection/powder/mantid_projection_calculator.py
+++ models/projection/powder/mantid_projection_calculator.py
@@ -1,16 +1,35 @@
-from mantid.simpleapi import ConvertToMD
+from mantid.simpleapi import ConvertToMD, SliceMD
 from models.projection.powder.projection_calculator import ProjectionCalculator
+from models.workspacemanager.mantid_workspace_provider import MantidWorkspaceProvider
 
 
-MD_SUFFIX = '_QE'
 
 
 class MantidProjectionCalculator(ProjectionCalculator):
+    def __init__(self):
+        self._workspace_provider = MantidWorkspaceProvider()
+
     def calculate_projection(self, input_workspace, axis1, axis2):
-        output_workspace = input_workspace + MD_SUFFIX
         if axis1 == '|Q|' and axis2 == 'Energy':
+            output_workspace = input_workspace + '_QE'
             ConvertToMD(InputWorkspace=input_workspace, OutputWorkspace=output_workspace, QDimensions='|Q|',
                         PreprocDetectorsWS='-')
+
+        elif axis1 == 'Energy' and axis2 == '|Q|':
+            output_workspace = input_workspace + '_EQ'
+            ConvertToMD(InputWorkspace=input_workspace, OutputWorkspace=output_workspace, QDimensions='|Q|',
+                        PreprocDetectorsWS='-')
+            output_workspace_handle = self._workspace_provider.get_workspace_handle(output_workspace)
+            # Now swapping dim0 and dim1
+            dim0 = output_workspace_handle.getDimension(1)
+            dim1 = output_workspace_handle.getDimension(0)
+            # format into dimension string as expected
+            dim0 = dim0.getName() + ',' + str(dim0.getMinimum()-1) + ',' +\
+                   str(dim0.getMaximum() + 1) + ',' + str(dim0.getNBins())
+            dim1 = dim1.getName() + ',' + str(dim1.getMinimum()-1) + ',' +\
+                   str(dim1.getMaximum() + 1) + ',' + str(dim1.getNBins())
+            SliceMD(InputWorkspace=output_workspace, OutputWorkspace=output_workspace, AlignedDim0=dim0, AlignedDim1=dim1)
+
         else:
-            raise NotImplementedError('MSlice currently only supports projection to Energy vs |Q|')
+            raise NotImplementedError("Not implemented axis1 = %s and axis2 = %s" % (axis1, axis2))
 
