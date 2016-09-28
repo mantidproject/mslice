854f24b983b5b432ea7680d05e2411111f33be62
Martyn Gigg <martyn.gigg@gmail.com>
2016-09-28 17:06:38 +0100

    Fix remaining flake8 warnings

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,9 +1,9 @@
 """Defines the command-line interface to MSlice
 """
 import plotting.pyplot as _plt
-from plotting.pyplot import *
+from plotting.pyplot import * # noqa: F401
 import _mslice_commmands
-from _mslice_commmands import *
+from _mslice_commmands import * # noqa: F401
 
 # Define names imported by * imports
 __all__ = dir(_plt) + dir(_mslice_commmands)
