fde27241e420060f6b81ff0045b104ed8ff676ca
Martyn Gigg <martyn.gigg@gmail.com>
2016-11-16 11:31:10 +0000

    Simplify build process.
    Remove new custom build-qt command in favour of overriding the default
    build_py command.
    Refs #102

--- setup.py
+++ setup.py
@@ -9,8 +9,6 @@ from __future__ import print_function
 
 import os
 from setuptools.command.build_py import build_py as _build_py
-from setuptools.command.install import install as _install
-from setuptools.command.install_lib import install_lib
 from setuptools import find_packages, setup
 from subprocess import check_call
 import sys
@@ -48,17 +46,17 @@ def get_data_files():
     return data_files
 
 # ==============================================================================
-# Custom distutils commands
+# Custom distutils build & install commands
 # ==============================================================================
 
-class build_qt(_build_py):
-    description = "build every qt related resources (.uic and .qrc and .pyc)"
+class build_py(_build_py):
+    description = "build pure python + qt related resources (.uic and .qrc and .pyc)"
 
     PACKAGE = NAME
 
     def finalize_options(self):
         _build_py.finalize_options(self)
-        self.packages = [NAME]
+        self.distribution.packages.append(NAME)
 
     def compile_src(self, src, dest):
         compiler = self.get_compiler(src)
@@ -73,13 +71,15 @@ class build_qt(_build_py):
             sys.stderr.write('[Error] %r\n' % str(e))
 
     def run(self):
+        from distutils.dep_util import newer
         for dirpath, _, filenames in os.walk(self.get_package_dir(self.PACKAGE)):
             package = dirpath.split(os.sep)
             for filename in filenames:
                 module = self.get_module_name(filename)
                 module_file = self.get_module_outfile(self.build_lib, package, module)
                 src_file = os.path.join(dirpath, filename)
-                self.compile_src(src_file, module_file)
+                if newer(src_file, module_file):
+                    self.compile_src(src_file, module_file)
         _build_py.run(self)
 
     @staticmethod
@@ -101,44 +101,6 @@ class build_qt(_build_py):
     def get_module_name(src_filename):
         name, ext = os.path.splitext(src_filename)
         return {'.qrc': '%s_rc', '.ui': '%s_ui'}.get(ext, '%s') % name
-
-
-class build_py(_build_py):
-
-    def run(self):
-        self.run_command('build_qt')
-        self.distribution.packages.append(NAME)
-        _build_py.run(self)
-
-    # 'sub_commands': a list of commands this command might have to run to
-    # get its work done.  See cmd.py for more info.
-    sub_commands = [('build_qt', None)] + _build_py.sub_commands
-
-
-class install_qt(install_lib):
-    description = "install the qt interface resources"
-
-    def run(self):
-        if not self.skip_build:
-            self.run_command('build_qt')
-
-        self.distribution.packages.append(NAME)
-        install_lib.run(self)
-
-
-class install(_install):
-
-    def run(self):
-        if not self.skip_build:
-            self.run_command('build_qt')
-
-        self.distribution.packages.append(NAME)
-        _install.run(self)
-
-    # 'sub_commands': a list of commands this command might have to run to
-    # get its work done.  See cmd.py for more info.
-    sub_commands = [('install_qt', None)] + _install.sub_commands
-
 
 # ==============================================================================
 # Setup arguments
@@ -162,10 +124,7 @@ setup_args = dict(name=NAME,
                                'Programming Language :: Python :: 2.7',
                                'Development Status :: 4 - Beta',
                                'Topic :: Scientific/Engineering'],
-                  cmdclass={'build_qt': build_qt,
-                            'build_py': build_py,
-                            'install_qt': install_qt,
-                            'install': install})
+                  cmdclass={'build_py': build_py})
 
 # ==============================================================================
 # Setuptools deps
