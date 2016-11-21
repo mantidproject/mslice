d3213b853afc8ea62ea5295cbd4511beab4c6dd4
Martyn Gigg <martyn.gigg@gmail.com>
2016-11-21 14:18:17 +0000

    Reduce verbosity of Windows devel script
    Also exits on failure of build step.
    Refs #102

--- mslicedevel.bat
+++ mslicedevel.bat
@@ -3,12 +3,15 @@
 :: It assumes that mantid is installed in the standard
 :: location.
 ::
-set THIS_DIR=%~dp0
-set PYTHON_EXE=C:\MantidInstall\bin\python.exe
+@set THIS_DIR=%~dp0
+@set PYTHON_EXE=C:\MantidInstall\bin\python.exe
 
 :: Build and run mslice
-set BUILD_DIR=%THIS_DIR%build
+@set BUILD_DIR=%THIS_DIR%build
 %PYTHON_EXE% %THIS_DIR%setup.py build
-set PYTHONPATH=%BUILD_DIR%\lib;%PYTHONPATH%
-set PY_VERS=2.7
+@if %ERRORLEVEL% NEQ 0 (
+  exit /B %ERRORLEVEL%
+)
+@set PYTHONPATH=%BUILD_DIR%\lib;%PYTHONPATH%
+@set PY_VERS=2.7
 %BUILD_DIR%\scripts-%PY_VERS%\mslice.bat
