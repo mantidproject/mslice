53eef2b8586a1f280a84364cdf0d5c3ae31023aa
Martyn Gigg <martyn.gigg@gmail.com>
2016-11-21 14:08:45 +0000

    Add devel script for Windows
    Refs #102

--- setup.py
+++ setup.py
@@ -161,7 +161,12 @@ setup_args['tests_require'] = read_requirements_from_file(os.path.join(THIS_DIR,
 
 # Startup scripts - these use the mantidpython wrappers so we cannot
 # go through the entry_points mechanism
-setup_args['scripts'] = ['scripts/mslice', 'scripts/start_mslice.py']
+scripts =  ['scripts/start_mslice.py']
+if os.name == 'nt':
+    scripts.append('scripts/mslice.bat')
+else:
+    scripts.append('scripts/mslice')
+setup_args['scripts'] = scripts
 
 #==============================================================================
 # Main setup
