03800fcb7e64b5be9821948732fa3c8af32bc7ea
Martyn Gigg <martyn.gigg@gmail.com>
2016-11-29 10:22:21 +0000

    Fix PYTHONPATH to local build on RHEL
    Refs #102

--- mslicedevel.sh
+++ mslicedevel.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 #
-# Basic wrapper script for running in development mode
+# Basic wrapper script for running in development mode. It assumes the current
+# working directory is the directory containing this script.
 #
 PY_VERS=2.7
 
 python setup.py build
-PYTHONPATH=$PWD/build/lib.linux-$(uname -p)-${PY_VERS}:$PYTHONPATH
-build/scripts-${PY_VERS}/mslice
+# The lib directory can contain the os and architecture
+if [ -d "build/lib" ]; then
+  LOCAL_PYTHONPATH=$PWD/build/lib
+else
+  LOCAL_PYTHONPATH=$PWD/build/lib.linux-$(uname -p)-${PY_VERS}
+fi
+PYTHONPATH=$LOCAL_PYTHONPATH:$PYTHONPATH build/scripts-${PY_VERS}/mslice
