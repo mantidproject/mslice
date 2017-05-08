84d13ccd15001aff14566cf2ef8fb3b88839f390
Duc Le <duc.le@stfc.ac.uk>
2017-05-08 15:18:12 +0100

    Re #42 - gives a warning on old matplotlib versions

--- mslice/app/__init__.py
+++ mslice/app/__init__.py
@@ -11,10 +11,8 @@ def check_mpl():
     import matplotlib
     if LooseVersion(matplotlib.__version__) < LooseVersion("1.5.0"):
         import warnings
-        warnings.warn('')
-        warnings.warn('A version of Matplotlib older than 1.5.0 has been detected.', ImportWarning)
-        warnings.warn('Some features of MSlice may not work correctly.', ImportWarning)
-        warnings.warn('')
+        warnings.warn('A version of Matplotlib older than 1.5.0 has been detected.')
+        warnings.warn('Some features of MSlice may not work correctly.')
         global MPL_COMPAT
         MPL_COMPAT = True
 
