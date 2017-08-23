fffba8ee2fd9cca99db1ae675addfa9e4ac3d057
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2017-08-23 09:48:37 +0100

    wrapper class skeletons Re #151

---
+++ mslice/workspace/pixel_workspace.py
@@ -0,0 +1,4 @@
+from workspace import Workspace
+
+class PixelWorkspace(Workspace):
+    pass
---
+++ mslice/workspace/workspace.py
@@ -0,0 +1,36 @@
+
+
+class Workspace(object):
+
+    def __init__(self):
+        pass
+
+    def get_coordinates(self):
+        pass
+
+    def get_signal(self):
+        pass
+
+    def get_error(self):
+        pass
+
+    def get_variance(self):
+        pass
+
+    def __add__(self, other):
+        pass
+
+    def __sub__(self, other):
+        pass
+
+    def __mul__(self,other):
+        pass
+
+    def __div__(self, other):
+        pass
+
+    def __pow__(self, power):
+        pass
+
+    def __neg__(self):
+        pass
