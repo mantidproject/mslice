6f7b235ef4104686ae551262ab7db7ab6708ea70
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2017-08-23 17:05:23 +0100

    add pixel_workspace methods Re #151

--- mslice/workspace/histogram_workspace.py
+++ mslice/workspace/histogram_workspace.py
@@ -9,14 +9,14 @@ class HistogramWorkspace(Workspace):
 
     def get_coordinates(self):
         nevents = self.histogram_workspace.getNumEventsArray()
-        return {x: (self.histogram_workspace.signalAt(x) / nevents[x])[0] for x in range(self.histogram_workspace.getNPoints()) }
+        return {x: (self.histogram_workspace.signalAt(x) / nevents[x])[0] for x in range(self.histogram_workspace.getNPoints())}
         # ??? not sure of requirements here
 
     def get_signal(self):
         return self.histogram_workspace.getSignalArray()
 
     def get_error(self):
-        np.sqrt(self.get_variance())
+        return np.sqrt(self.get_variance())
 
     def get_variance(self):
         return self.histogram_workspace.getErrorSquaredArray()
--- mslice/workspace/pixel_workspace.py
+++ mslice/workspace/pixel_workspace.py
@@ -1,7 +1,39 @@
 from workspace import Workspace
-from mantid.api import IMDEventWorkspace
+from histogram_workspace import HistogramWorkspace
+from mantid.simpleapi import BinMD
+
 
 class PixelWorkspace(Workspace):
+
     def __init__(self, pixel_workspace):
-        pws = IMDEventWorkspace()
-        # pws.
+        self.pixel_workspace = pixel_workspace
+        self.histogram_workspace = None
+
+    def get_histogram_workspace(self):
+        if self.histogram_workspace is None:
+            dim_values = []
+            for x in range(6):
+                try:
+                    dim = self.pixel_workspace.getDimension(x)
+                    dim_info = dim.getName() + ',' + str(dim.getMinimum()) + ',' + str(dim.getMaximum()) + ',' + str(10)
+                except RuntimeError:
+                    dim_info = None
+                dim_values.append(dim_info)
+            histo_workspace = BinMD(InputWorkspace=self.pixel_workspace, OutputWorkspace="output",
+                                    AlignedDim0=dim_values[0], alignedDim1=dim_values[1],
+                                    alignedDim2=dim_values[2], alignedDim3=dim_values[3],
+                                    alignedDim4=dim_values[4], alignedDim5=dim_values[5])
+            self.histogram_workspace = HistogramWorkspace(histo_workspace)
+        return self.histogram_workspace
+
+    def get_coordinates(self):
+        pass
+
+    def get_signal(self):
+        return self.get_histogram_workspace().get_signal()
+
+    def get_error(self):
+        return self.get_histogram_workspace().get_error()
+
+    def get_variance(self):
+        return self.get_histogram_workspace().get_variance()
