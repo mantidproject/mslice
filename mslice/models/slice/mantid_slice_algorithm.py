860c409bd75cf1a840cfa022be7e890a02cf7202
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2017-11-09 15:19:42 +0000

    defer calculation, compatible with edited title re #164

--- mslice/models/slice/mantid_slice_algorithm.py
+++ mslice/models/slice/mantid_slice_algorithm.py
@@ -9,6 +9,8 @@ from .slice_algorithm import SliceAlgorithm
 from mslice.models.alg_workspace_ops import AlgWorkspaceOps
 from mslice.models.workspacemanager.mantid_workspace_provider import MantidWorkspaceProvider
 
+KB_MEV = constants.value('Boltzmann constant in eV/K') * 1000
+
 
 class MantidSliceAlgorithm(AlgWorkspaceOps, SliceAlgorithm):
     def __init__(self):
@@ -16,7 +18,7 @@ class MantidSliceAlgorithm(AlgWorkspaceOps, SliceAlgorithm):
 
     def compute_slice(self, selected_workspace, x_axis, y_axis, smoothing, norm_to_one, sample_temp):
         workspace = self._workspace_provider.get_workspace_handle(selected_workspace)
-        assert isinstance(workspace,IMDEventWorkspace)
+        assert isinstance(workspace, IMDEventWorkspace)
         self._fill_in_missing_input(x_axis, workspace)
         self._fill_in_missing_input(y_axis, workspace)
         n_x_bins = self._get_number_of_steps(x_axis)
@@ -37,20 +39,18 @@ class MantidSliceAlgorithm(AlgWorkspaceOps, SliceAlgorithm):
         boundaries = [x_axis.start, x_axis.end, y_axis.start, y_axis.end]
         if norm_to_one:
             plot_data = self._norm_to_one(plot_data)
-        plot = [None, None, None]
-        plot[0] = plot_data
-        plot[1] = self.compute_chi(plot_data, sample_temp, y_axis)
-        plot[2] = self.compute_chi_magnetic(plot[1])
+        plot = [plot_data, None, None]
         return plot, boundaries
 
     def compute_chi(self, scattering_data, sample_temp, y_axis):
         if sample_temp is None:
             return None
-        kBT = sample_temp * constants.value('Boltzmann constant in eV/K') * 1000  # convert to meV/K
+        kBT = sample_temp * KB_MEV
         energy_transfer = np.arange(y_axis.start, y_axis.end, y_axis.step)
         signs = np.sign(energy_transfer)
+        signs[signs == 0] = 1
         boltzmann_dist = np.exp(-energy_transfer / kBT)
-        chi = (signs + (boltzmann_dist * -signs))[:,None]
+        chi = (signs + (boltzmann_dist * -signs))[:, None]
         chi = np.pi * chi * scattering_data
         return chi
 
@@ -60,6 +60,36 @@ class MantidSliceAlgorithm(AlgWorkspaceOps, SliceAlgorithm):
         # 291 milibarns is the total neutron cross-section for a moment of one bohr magneton
         chi_magnetic = chi / 291
         return chi_magnetic
+
+    def sample_temperature(self, ws_name, sample_temp_fields):
+        if ws_name[-3:] == '_QE':
+            ws_name = ws_name[:-3]  # mantid drops log data during projection, need unprojected workspace.
+        ws = self._workspace_provider.get_workspace_handle(ws_name)
+        sample_temp = None
+        for field_name in sample_temp_fields:
+            try:
+                sample_temp = ws.run().getLogData(field_name).value
+            except RuntimeError:
+                pass
+        try:
+            float(sample_temp)
+        except (ValueError, TypeError):
+            pass
+        else:
+            return sample_temp
+        if isinstance(sample_temp, str):
+            sample_temp = self.get_sample_temperature_from_string(sample_temp)
+        if isinstance(sample_temp, np.ndarray) or isinstance(sample_temp, list):
+            sample_temp = np.mean(sample_temp)
+        return sample_temp
+
+    def get_sample_temperature_from_string(self, string):
+        pos_k = string.find('K')
+        if pos_k == -1:
+            return None
+        k_string = string[pos_k - 3:pos_k]
+        sample_temp = float(''.join(c for c in k_string if c.isdigit()))
+        return sample_temp
 
     def _norm_to_one(self, data):
         data_range = data.max() - data.min()
