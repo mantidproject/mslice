fix calculation, add recoil line legend re #163

mslice/models/slice/mantid_slice_algorithm.py
@@ -122,7 +122,8 @@ class MantidSliceAlgorithm(AlgWorkspaceOps, SliceAlgorithm):
 
     def compute_recoil_line(self, x_axis, y_axis, relative_mass=1):
         momentum_transfer = np.arange(x_axis.start, x_axis.end, x_axis.step)
-        line = np.square(momentum_transfer * HBAR_MEV) / (2 * relative_mass * constants.neutron_mass)
+        line = np.square(momentum_transfer * 1.e10 * constants.hbar ) / (2 * relative_mass * constants.neutron_mass) / \
+               (constants.elementary_charge / 1000)
         return momentum_transfer, line
 
     def _norm_to_one(self, data):