3781d1cbdad4a69d3b1a4c4f7d850d0d080527e0
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2018-01-10 10:55:47 -0500

    remove build step from .sh script

--- mslicedevel.sh
+++ mslicedevel.sh
@@ -3,5 +3,5 @@
 # Basic wrapper script for running in development mode. It assumes the current
 # working directory is the directory containing this script.
 #
-env QT_API=pyqt python setup.py build_py --inplace
+env QT_API=pyqt python setup.py
 env PYTHONPATH=$(dirname $0):$PYTHONPATH /bin/bash scripts/mslice
