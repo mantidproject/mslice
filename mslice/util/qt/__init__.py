2059fe052659d27a5f9c6a56f239c0fafad88386
Duc Le <duc.le@stfc.ac.uk>
2018-01-15 16:34:28 +0000

    Re #198 - typo

--- mslice/util/qt/__init__.py
+++ mslice/util/qt/__init__.py
@@ -2,4 +2,4 @@ from .functions import load_ui
 try:
     from qtpy import QT_VERSION as QT_VERSION
 except (ImportError, ValueError):
-    from pyqt4.Qt import QT_VERSION_STR as QT_VERSION
+    from PyQt4.Qt import QT_VERSION_STR as QT_VERSION
