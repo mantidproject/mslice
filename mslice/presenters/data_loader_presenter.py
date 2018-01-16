93e1520ce833248f6ce0cb0a1272f6d85be5cc9a
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2018-01-16 10:48:38 +0000

    Workspaces can be loaded and passed to manager re #206

--- mslice/presenters/data_loader_presenter.py
+++ mslice/presenters/data_loader_presenter.py
@@ -7,13 +7,14 @@ from mslice.presenters.presenter_utility import PresenterUtility
 
 class DataLoaderPresenter(PresenterUtility): #TODO: create interface
 
-    def __init__(self, data_loader_view):
-        self._main_presenter = None
-        self._view = data_loader_view
-        self._workspace_provider = MantidWorkspaceProvider()
 
-    def get_workspace_provider(self):
-        return self._workspace_provider
+    def __init__(self, data_loader_view):
+        self._view = data_loader_view
+        self._main_presenter = None
+        self._workspace_provider = None
+
+    def set_workspace_provider(self, workspace_provider):
+        self._workspace_provider = workspace_provider
 
     def load_workspace(self, workspace_to_load):
         workspace_to_load = [str(workspace_to_load)] #TODO: tmp until multiple can be loaded
@@ -30,7 +31,7 @@ class DataLoaderPresenter(PresenterUtility): #TODO: create interface
                 continue
             try:
                 print(workspace_to_load)
-                self._workspace_provider.load(filename=[workspace_to_load][ii], output_workspace=ws_name)
+                self._workspace_provider.load(filename=workspace_to_load[ii], output_workspace=ws_name)
             except RuntimeError:
                 not_opened.append(ws_name)
             else:
@@ -38,16 +39,31 @@ class DataLoaderPresenter(PresenterUtility): #TODO: create interface
                 # Checks if this workspace has efixed set. If not, prompts the user and sets it.
                 if self._workspace_provider.get_EMode(ws_name) == 'Indirect' and not self._workspace_provider.has_efixed(ws_name):
                     if not allChecked:
-                        Ef, allChecked = self._workspace_manager_view.get_workspace_efixed(ws_name, multi)
+                        Ef, allChecked = self._view.get_workspace_efixed(ws_name, multi)
                     self._workspace_provider.set_efixed(ws_name, Ef)
 
-        # self._report_load_errors(ws_names, not_opened, not_loaded)
-        self._workspace_manager_view.display_loaded_workspaces(self._workspace_provider.get_workspace_names())
-        self._workspace_manager_view.set_workspace_selected(
-            [self._workspace_manager_view.get_workspace_index(ld_name) for ld_name in loaded])
+        self._report_load_errors(ws_names, not_opened, not_loaded)
+        self._main_presenter.update_displayed_workspaces()
 
     def _confirm_workspace_overwrite(self, ws_name):
         if ws_name in self._workspace_provider.get_workspace_names():
-            return self._workspace_manager_view.confirm_overwrite_workspace()
+            return self._view.confirm_overwrite_workspace()
         else:
             return True
+
+    def _report_load_errors(self, ws_names, not_opened, not_loaded):
+        if len(not_opened) == len(ws_names):
+            self._view.error_unable_to_open_file()
+            return
+        elif len(not_opened) > 0:
+            errmsg = not_opened[0] if len(not_opened) == 1 else ",".join(not_opened)
+            self._view.error_unable_to_open_file(errmsg)
+        if len(not_loaded) == len(ws_names):
+            self._view.no_workspace_has_been_loaded()
+            return
+        elif len(not_loaded) > 0:
+            errmsg = not_loaded[0] if len(not_loaded) == 1 else ",".join(not_loaded)
+            self._view.no_workspace_has_been_loaded(errmsg)
+
+    def workspace_selection_changed(self):
+        pass
