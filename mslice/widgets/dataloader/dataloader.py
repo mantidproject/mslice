ae6af11668c53d6b9d6908287e34da2a68c353a7
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2018-01-18 10:52:49 +0000

    Modify UI, add home/sort controls

--- mslice/widgets/dataloader/dataloader.py
+++ mslice/widgets/dataloader/dataloader.py
@@ -4,8 +4,8 @@ import os
 
 from functools import partial
 
-from qtpy.QtWidgets import QWidget, QFileSystemModel, QAbstractItemView, QMessageBox
-from qtpy.QtCore import Signal, QDir
+from mslice.util.qt.QtWidgets import QWidget, QFileSystemModel, QAbstractItemView, QMessageBox
+from mslice.util.qt.QtCore import Signal, QDir
 
 from mslice.presenters.data_loader_presenter import DataLoaderPresenter
 from mslice.util.qt import load_ui
@@ -21,9 +21,12 @@ class DataLoaderWidget(QWidget): # and some view interface
         load_ui(__file__, 'dataloader.ui', self)
 
         self.file_system = QFileSystemModel()
-        self.directory = QDir("C:/")
-        self.file_system.setRootPath(self.directory.absolutePath())
+        self.directory = QDir(os.path.expanduser('~'))
+        path = self.directory.absolutePath()
+        self.file_system.setRootPath(path)
         self.table_view.setModel(self.file_system)
+        self.table_view.setRootIndex(self.file_system.index(path))
+        self.txtpath.setText(path)
         self.table_view.setColumnWidth(0, 320)
         self.table_view.setColumnWidth(1, 0)
         self.table_view.setColumnWidth(3, 0)
@@ -31,7 +34,6 @@ class DataLoaderWidget(QWidget): # and some view interface
         self._presenter = DataLoaderPresenter(self)
 
         self.table_view.doubleClicked.connect(self.clicked)
-        self.btnrefresh.clicked.connect(self.refresh)
         self.txtpath.editingFinished.connect(self.refresh)
         self.btnback.clicked.connect(self.back)
         self.btnload.clicked.connect(partial(self.load, False))
