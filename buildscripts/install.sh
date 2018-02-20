b290741fddc36c1b488266fb1f40ad0d0eaef0d6
Martyn Gigg <martyn.gigg@gmail.com>
2018-02-20 15:03:09 +0000

    Move xvfb installation to use sudo

--- buildscripts/install.sh
+++ buildscripts/install.sh
@@ -2,5 +2,3 @@
 
 # Install mslice requirements
 pip install -r setup-requirements.txt -r install-requirements.txt -r test-requirements.txt coveralls
-# Install X virtual framebuffer to emulate X server
-apt-get install -y xvfb
