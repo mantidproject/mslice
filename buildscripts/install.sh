05a98041441dc1c597905af14760c774a6bb01a2
Martyn Gigg <martyn.gigg@gmail.com>
2018-02-20 13:53:29 +0000

    Use xvfb for running tests to emulate X server
    This will allow use to use QtTest in the future.

--- buildscripts/install.sh
+++ buildscripts/install.sh
@@ -2,3 +2,5 @@
 
 # Install mslice requirements
 pip install -r setup-requirements.txt -r install-requirements.txt -r test-requirements.txt coveralls
+# Install X virtual framebuffer to emulate X server
+apt-get install -y xvfb
