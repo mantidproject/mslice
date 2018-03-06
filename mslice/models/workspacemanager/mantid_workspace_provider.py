6b8e429b3a38c9a43e442d764c6c8018f7e6e6f7
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2018-03-06 16:47:32 +0000

    account for dimensions on saving slices re #255

--- mslice/models/workspacemanager/mantid_workspace_provider.py
+++ mslice/models/workspacemanager/mantid_workspace_provider.py
@@ -24,6 +24,64 @@ from mslice.presenters.slice_plotter_presenter import Axis
 # Classes and functions
 # -----------------------------------------------------------------------------
 
+def _output_data_to_ascii(output_path, out_data, header):
+    np.savetxt(str(output_path), out_data, fmt='%12.9e', header=header)
+
+
+def _get_mdhisto_xye(histo_ws):
+    dim = histo_ws.getDimension(0)
+    start = dim.getMinimum()
+    end = dim.getMaximum()
+    step = dim.getBinWidth()
+    x = np.arange(start, end, step)
+    y = histo_ws.getSignalArray()
+    e = np.sqrt(histo_ws.getErrorSquaredArray())
+    return x, y, e
+
+
+def _get_slice_mdhisto_xye(histo_ws):
+    dim_sz = [histo_ws.getDimension(i).getNBins() for i in range(histo_ws.getNumDims())]
+    nz_dim = [i for i, v in enumerate(dim_sz) if v > 1]
+    numdim = len(nz_dim)
+    x = []
+    for i in nz_dim:
+        dim = histo_ws.getDimension(i)
+        start = dim.getMinimum()
+        end = dim.getMaximum()
+        nshape = [1] * numdim
+        nshape[i] = dim_sz[i]
+        x.append(np.reshape(np.linspace(start, end, dim_sz[i]), tuple(nshape)))
+    y = histo_ws.getSignalArray()
+    e = np.sqrt(histo_ws.getErrorSquaredArray())
+    nbins = np.prod(dim_sz)
+    x = [np.reshape(x0, nbins) for x0 in np.broadcast_arrays(*x)]
+    y = np.reshape(y, nbins)
+    e = np.reshape(e, nbins)
+    return x, y, e
+
+
+def _save_cut_to_ascii(workspace, ws_name, output_path):
+    # get integration ranges from the name
+    int_ranges = ws_name[ws_name.find('('):]
+    int_start = int_ranges[1:int_ranges.find(',')]
+    int_end = int_ranges[int_ranges.find(',')+1:-1]
+    ws_name = ws_name[:ws_name.find('_cut')]
+
+    dim = workspace.getDimension(0)
+    units = dim.getUnits()
+
+    x, y, e = _get_mdhisto_xye(workspace)
+    header = 'MSlice Cut of workspace "%s" along "%s" between %s and %s' % (ws_name, units, int_start, int_end)
+    out_data = np.c_[x, y, e]
+    _output_data_to_ascii(output_path, out_data, header)
+
+
+def _save_slice_to_ascii(workspace, output_path):
+    header = 'MSlice Slice of workspace "%s"' % (workspace.name())
+    x, y, e = _get_slice_mdhisto_xye(workspace)
+    out_data = np.column_stack(tuple(x+[y, e]))
+    _output_data_to_ascii(output_path, out_data, header)
+
 
 class MantidWorkspaceProvider(WorkspaceProvider):
     def __init__(self):
@@ -209,39 +267,25 @@ class MantidWorkspaceProvider(WorkspaceProvider):
     def _save_ascii(self, workspace, path):
         workspace_handle = self.get_workspace_handle(workspace)
         if isinstance(workspace_handle, IMDEventWorkspace):
-            workspace_handle = self.get_slice_MDHisto(workspace_handle, workspace)
-        if isinstance(workspace_handle, IMDHistoWorkspace):
-            self._save_cut_to_ascii(workspace_handle, workspace, path)
+            workspace_handle = self._get_slice_mdhisto(workspace_handle, workspace)
+            _save_slice_to_ascii(workspace_handle, path)
+        elif isinstance(workspace_handle, IMDHistoWorkspace):
+            _save_cut_to_ascii(workspace_handle, workspace, path)
         else:
             SaveAscii(InputWorkspace=workspace, Filename=path)
-
-    def _save_cut_to_ascii(self, workspace, ws_name, output_path):
-        # get integration ranges from the name
-        int_ranges = ws_name[ws_name.find('('):]
-        int_start = int_ranges[1:int_ranges.find(',')]
-        int_end = int_ranges[int_ranges.find(',')+1:-1]
-        ws_name = ws_name[:ws_name.find('_cut')]
-
-        dim = workspace.getDimension(0)
-        units = dim.getUnits()
-
-        x, y, e = self.get_md_histo_xye(workspace)
-        header = 'MSlice Cut of workspace "%s" along "%s" between %s and %s' % (ws_name, units, int_start, int_end)
-        out_data = np.c_[x, y, e]
-        np.savetxt(str(output_path), out_data, fmt='%12.9e', header=header)
 
     def _save_matlab(self, workspace, path):
         workspace_handle = self.get_workspace_handle(workspace)
         if isinstance(workspace_handle, IMDEventWorkspace):
-            workspace_handle = self.get_slice_MDHisto(workspace_handle, workspace)
-        if isinstance(workspace_handle, IMDHistoWorkspace):
-            x, y, e = self.get_md_histo_xye(workspace_handle)
+            workspace_handle = self._get_slice_mdhisto(workspace_handle, workspace)
+            x, y, e  = _get_slice_mdhisto_xye(workspace_handle)
+        elif isinstance(workspace_handle, IMDHistoWorkspace):
+            x, y, e = _get_mdhisto_xye(workspace_handle)
         else:
             x = workspace_handle.extractX()
             y = workspace_handle.extractY()
             e = workspace_handle.extractE()
-        mdict = {'x': x, 'y': y, 'e': e}
-        savemat(path, mdict=mdict)
+        savemat(path, mdict={'x': x, 'y': y, 'e': e})
 
     def load_from_ascii(self, file_path, ws_name):
         file = open(file_path, 'r')
@@ -255,7 +299,7 @@ class MantidWorkspaceProvider(WorkspaceProvider):
         CreateMDHistoWorkspace(SignalInput=y, ErrorInput=e, Dimensionality=1, Extents=extents, NumberOfBins=nbins,
                                Names='Dim1', Units=units, OutputWorkspace=ws_name)
 
-    def get_slice_MDHisto(self, workspace, ws_name):
+    def _get_slice_mdhisto(self, workspace, ws_name):
         from mslice.models.slice.mantid_slice_algorithm import MantidSliceAlgorithm
         try:
             return self.get_workspace_handle('__' + ws_name)
@@ -266,21 +310,10 @@ class MantidWorkspaceProvider(WorkspaceProvider):
             slice_alg.compute_slice(ws_name, x_axis, y_axis, False)
             return self.get_workspace_handle('__' + ws_name)
 
-
     def get_axis_from_dimension(self, workspace, ws_name, id):
         dim = workspace.getDimension(id).getName()
         min, max, step = self._limits[ws_name][dim]
         return Axis(dim, min, max, step)
-
-    def get_md_histo_xye(self, histo_ws):
-        dim = histo_ws.getDimension(0)
-        start = dim.getMinimum()
-        end = dim.getMaximum()
-        step = dim.getBinWidth()
-        x = np.arange(start, end, step)
-        y = histo_ws.getSignalArray()
-        e = np.sqrt(histo_ws.getErrorSquaredArray())
-        return x, y, e
 
     def is_pixel_workspace(self, workspace_name):
         from mantid.api import IMDEventWorkspace
