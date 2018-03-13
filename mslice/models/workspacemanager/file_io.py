542c71fe2a01b1dbbbc54c068a29ba4dd7b873c3
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2018-03-13 15:40:43 +0000

    fix error saving files

--- mslice/models/workspacemanager/file_io.py
+++ mslice/models/workspacemanager/file_io.py
@@ -32,8 +32,8 @@ def get_save_directory(multiple_files=False, save_as_image=False, default_ext=No
             ext_to_qtfilter = {'.nxs': 'Nexus (*.nxs)', '.txt': 'Ascii (*.txt)', '.mat': 'Matlab (*.mat)', }
             file_dialog.selectNameFilter(ext_to_qtfilter[default_ext])
         if (file_dialog.exec_()):
-            path = file_dialog.selectedFiles()[0]
-            if not path.rfind("."): # add extension unless there's one in the name
+            path = str(file_dialog.selectedFiles()[0])
+            if not '.' in path: # add extension unless there's one in the name
                 path += file_dialog.selectedFilter()[-5:-1]
             ext = path[path.rfind('.'):]
             return os.path.dirname(path), os.path.basename(path), ext
