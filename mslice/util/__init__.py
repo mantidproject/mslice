4d916fe6d4a59aa9ae873d67cc028b0d037e31bc
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2018-03-22 09:28:50 +0000

    flake8 re #291

--- mslice/util/__init__.py
+++ mslice/util/__init__.py
@@ -1,8 +1,9 @@
+from distutils.version import LooseVersion
+import matplotlib
+
 MPL_COMPAT = False
 
 # check matplotlib version
-from distutils.version import LooseVersion
-import matplotlib
 if LooseVersion(matplotlib.__version__) < LooseVersion("1.5.0"):
     import warnings
     warnings.warn('A version of Matplotlib older than 1.5.0 has been detected.')
