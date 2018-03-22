fa69201d69a5557ffb6cb935241835e0698ae45d
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2018-03-22 09:43:45 +0000

    remove unneeded global re #291

--- mslice/util/__init__.py
+++ mslice/util/__init__.py
@@ -8,5 +8,4 @@ if LooseVersion(matplotlib.__version__) < LooseVersion("1.5.0"):
     import warnings
     warnings.warn('A version of Matplotlib older than 1.5.0 has been detected.')
     warnings.warn('Some features of MSlice may not work correctly.')
-    global MPL_COMPAT
     MPL_COMPAT = True
