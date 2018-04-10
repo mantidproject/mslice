368929003f2db7d8a5ea5c380df1c638e284b524
Matthew Bowles <matthew.bowles@stfc.ac.uk>
2018-04-10 14:38:07 +0100

    add tests for powder/recoil lines

--- mslice/tests/slice_algorithm_test.py
+++ mslice/tests/slice_algorithm_test.py
@@ -22,6 +22,7 @@ class SliceAlgorithmTest(unittest.TestCase):
         cls.slice_alg = MantidSliceAlgorithm()
         cls.e_axis = Axis('DeltaE', -10, 15, 1)
         cls.q_axis = Axis('|Q|', 0.1, 3.1, 0.1)
+        cls.q_axis_degrees = Axis('Degrees', 3, 33, 1)
 
     def test_boltzmann_dist(self):
         e_axis = np.arange(self.e_axis.start, self.e_axis.end, self.e_axis.step)
@@ -71,3 +72,63 @@ class SliceAlgorithmTest(unittest.TestCase):
         self.assertAlmostEqual(gdos[0][0], 0.0, 6)
         self.assertAlmostEqual(gdos[10][15], 0.697758, 6)
         self.assertAlmostEqual(gdos[29][24], 2246.999938, 6)
+
+    @patch('mslice.models.slice.mantid_slice_algorithm.MantidWorkspaceProvider.get_EFixed')
+    @patch('mslice.models.slice.mantid_slice_algorithm.MantidWorkspaceProvider.get_EMode')
+    @patch('mslice.models.slice.mantid_slice_algorithm.MantidWorkspaceProvider.get_workspace_handle')
+    def test_recoil_line(self, ws_handle_mock, emode_mock, efixed_mock):
+        emode_mock.return_value = 'Direct'
+        efixed_mock.return_value = 20
+        x_axis, line = self.slice_alg.compute_recoil_line('ws_name', self.q_axis)
+        self.assertEqual(len(line), 30)
+        self.assertAlmostEqual(line[0], 0.020721, 6)
+        self.assertAlmostEqual(line[10], 2.507271, 6)
+        self.assertAlmostEqual(line[29], 18.649123, 6)
+
+    @patch('mslice.models.slice.mantid_slice_algorithm.MantidWorkspaceProvider.get_EFixed')
+    @patch('mslice.models.slice.mantid_slice_algorithm.MantidWorkspaceProvider.get_EMode')
+    @patch('mslice.models.slice.mantid_slice_algorithm.MantidWorkspaceProvider.get_workspace_handle')
+    def test_recoil_line_mass(self, ws_handle_mock, emode_mock, efixed_mock):
+        emode_mock.return_value = 'Direct'
+        efixed_mock.return_value = 20
+        x_axis, line = self.slice_alg.compute_recoil_line('ws_name', self.q_axis, 4)
+        self.assertEqual(len(line), 30)
+        self.assertAlmostEqual(line[0], 0.005180, 6)
+        self.assertAlmostEqual(line[10], 0.626818, 6)
+        self.assertAlmostEqual(line[29], 4.662281, 6)
+
+    @patch('mslice.models.slice.mantid_slice_algorithm.MantidWorkspaceProvider.get_EFixed')
+    @patch('mslice.models.slice.mantid_slice_algorithm.MantidWorkspaceProvider.get_EMode')
+    @patch('mslice.models.slice.mantid_slice_algorithm.MantidWorkspaceProvider.get_workspace_handle')
+    def test_recoil_line_degrees(self, ws_handle_mock, emode_mock, efixed_mock):
+        emode_mock.return_value = 'Direct'
+        efixed_mock.return_value = 20
+        x_axis, line = self.slice_alg.compute_recoil_line('ws_name', self.q_axis_degrees)
+        self.assertEqual(len(line), 30)
+        self.assertAlmostEqual(line[0], 0.054744, 6)
+        self.assertAlmostEqual(line[10], 0.999578, 6)
+        self.assertAlmostEqual(line[29], 5.276328, 6)
+
+    @patch('mslice.models.slice.mantid_slice_algorithm.MantidWorkspaceProvider.get_EFixed')
+    @patch('mslice.models.slice.mantid_slice_algorithm.MantidWorkspaceProvider.get_workspace_handle')
+    def test_powder_line(self, ws_handle_mock, efixed_mock):
+        efixed_mock.return_value = 20
+        x, y = self.slice_alg.compute_powder_line('ws_name', Axis('|Q|', 0.1, 9.1, 0.1), 'Copper')
+        self.assertEqual(len(x), len(y))
+        self.assertAlmostEqual(x[0], 3.010539, 6)
+        self.assertAlmostEqual(x[10], 5.764743, 6)
+        self.assertTrue(np.isnan(x[29]))
+        self.assertEqual(y[0], 1)
+        self.assertEqual(y[1], -1)
+
+    @patch('mslice.models.slice.mantid_slice_algorithm.MantidWorkspaceProvider.get_EFixed')
+    @patch('mslice.models.slice.mantid_slice_algorithm.MantidWorkspaceProvider.get_workspace_handle')
+    def test_powder_line_degrees(self, ws_handle_mock, efixed_mock):
+        efixed_mock.return_value = 20
+        x, y = self.slice_alg.compute_powder_line('ws_name', Axis('Degrees', 3, 93, 1), 'Copper')
+        self.assertEqual(len(x), len(y))
+        self.assertAlmostEqual(x[0], 57.961394, 6)
+        self.assertAlmostEqual(x[4], 68.038257, 6)
+        self.assertTrue(np.isnan(x[5]))
+        self.assertEqual(y[0], 1)
+        self.assertEqual(y[1], -1)
