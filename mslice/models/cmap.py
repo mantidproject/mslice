9bfaf3d72a3db5f5ce00456322ce5bd0376aa20b
Matthew <matthew.bowles@stfc.ac.uk>
2018-08-21 16:39:02 +0100

    remove unused colourmaps method re #325

--- mslice/models/cmap.py
+++ mslice/models/cmap.py
@@ -8,11 +8,3 @@ if not MPL_COMPAT:
     ALLOWED_CMAPS.insert(0, 'viridis')
 
 DEFAULT_CMAP = ALLOWED_CMAPS[0]
-
-
-def allowed_cmaps():
-    """Return the allowed list of colour maps"""
-    cmaps = ['viridis', 'jet', 'summer', 'winter', 'coolwarm']
-    if MPL_COMPAT:
-        cmaps.remove('viridis')
-    return cmaps
