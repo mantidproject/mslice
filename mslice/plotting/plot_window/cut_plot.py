49512f429b42f57b2cab3fec4254c05870f120c4
Andre Bamidele <aybamidele@gmail.com>
2018-09-05 11:05:38 +0100

    #370 can now modify the log range

--- mslice/plotting/plot_window/cut_plot.py
+++ mslice/plotting/plot_window/cut_plot.py
@@ -100,16 +100,14 @@ class CutPlot(IPlot):
     def change_axis_scale(self, xy_config):
         current_axis = self._canvas.figure.gca()
         if xy_config['x_log']:
-            xdata = [ll.get_xdata() for ll in current_axis.get_lines()]
-            xmin = get_min(xdata, absolute_minimum=0.)
+            xmin = xy_config['x_range'][0]
             current_axis.set_xscale('symlog', linthreshx=pow(10, np.floor(np.log10(xmin))))
             if xmin > 0:
                 xy_config['x_range'] = (xmin, xy_config['x_range'][1])
         else:
             current_axis.set_xscale('linear')
         if xy_config['y_log']:
-            ydata = [ll.get_ydata() for ll in current_axis.get_lines()]
-            ymin = get_min(ydata, absolute_minimum=0.)
+            ymin = xy_config['y_range'][0]
             current_axis.set_yscale('symlog', linthreshy=pow(10, np.floor(np.log10(ymin))))
             if ymin > 0:
                 xy_config['y_range'] = (ymin, xy_config['y_range'][1])
