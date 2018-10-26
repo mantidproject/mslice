fc5080f5a187cc42ace65ff037ade6d20c31c699
Andre Bamidele <aybamidele@gmail.com>
2018-10-26 17:03:21 +0100

    flake8 fixes #393

--- mslice/models/labels.py
+++ mslice/models/labels.py
@@ -1,10 +1,10 @@
-'''Module to create axis and legend labels'''
+"""Module to create axis and legend labels"""
 
 from __future__ import (absolute_import, division, print_function)
 from mslice.util import MPL_COMPAT
 
 CUT_INTENSITY_LABEL = 'Signal/#Events'
-recoil_labels={1:'Hydrogen', 2:'Deuterium', 4:'Helium'}
+recoil_labels = {1: 'Hydrogen', 2: 'Deuterium', 4: 'Helium'}
 
 # unit labels for projections
 MOD_Q_LABEL = '|Q|'
@@ -22,7 +22,7 @@ def get_display_name(axisUnits, comment=None):
         else:
             return 'Energy Transfer ' + ('(cm$^{-1}$)' if (comment and 'wavenumber' in comment) else '(meV)')
     elif 'MomentumTransfer' in axisUnits or '|Q|' in axisUnits:
-        return '|Q| (recip. Ang.)' if MPL_COMPAT else '$|Q|$ ($\mathrm{\AA}^{-1}$)'
+        return '|Q| (recip. Ang.)' if MPL_COMPAT else r'$|Q|$ ($\mathrm{\AA}^{-1}$)'
     elif '2Theta' in axisUnits:
         return 'Scattering Angle (degrees)' if MPL_COMPAT else r'Scattering Angle 2$\theta$ ($^{\circ}$)'
     else:
@@ -31,9 +31,9 @@ def get_display_name(axisUnits, comment=None):
 
 def generate_legend(workspace_name, integrated_dim, integration_start, integration_end):
     if MPL_COMPAT:
-        mappings = {'DeltaE':'E', 'MomentumTransfer':'|Q|', 'Degrees':r'2Theta'}
+        mappings = {'DeltaE': 'E', 'MomentumTransfer': '|Q|', 'Degrees': r'2Theta'}
     else:
-        mappings = {'DeltaE':'E', 'MomentumTransfer':'|Q|', 'Degrees':r'2$\theta$'}
+        mappings = {'DeltaE': 'E', 'MomentumTransfer': '|Q|', 'Degrees': r'2$\theta$'}
     integrated_dim = mappings[integrated_dim] if integrated_dim in mappings else integrated_dim
     return workspace_name + " " + "%.2f" % integration_start + "<" + integrated_dim + "<" + \
         "%.2f" % integration_end
