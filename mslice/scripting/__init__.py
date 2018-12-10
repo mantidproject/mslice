f354b1793b43416d59b2dddcb7d6bbbfd57ff32f
AndreBam <aybamidele@gmail.com>
2018-12-10 17:54:00 +0000

    used raw_ws instead of mslice wrapper to generate python script #385

--- mslice/scripting/__init__.py
+++ mslice/scripting/__init__.py
@@ -1,4 +1,7 @@
+from mantid.simpleapi import GeneratePythonScript
+from mslice.models.workspacemanager.workspace_provider import get_workspace_handle
 
 
-def generate_workspace_history():
-    pass
+def generate_script(ws_name):
+    ws = get_workspace_handle(ws_name).raw_ws
+    GeneratePythonScript(ws, Filename="test.py")
