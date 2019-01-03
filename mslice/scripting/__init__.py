81ca4a664ce20f4cef36376f7e3168a95d955595
Andre Bamidele <aybamidele@gmail.com>
2019-01-03 15:23:18 +0000

    added import statement to generate_script to allow the usage of cli commands to recreate plots upon script generation

--- mslice/scripting/__init__.py
+++ mslice/scripting/__init__.py
@@ -2,12 +2,33 @@ from mantid.simpleapi import GeneratePythonScript
 from mslice.models.workspacemanager.workspace_provider import get_workspace_handle
 from mslice.util.qt import QtWidgets
 
+PACKAGES = {'mslice.cli': 'mc', 'matplotlib.pyplot': 'plt'}
+
 
 def generate_script(figure, ws_name, filename=None):
     if filename is None:
         path = QtWidgets.QFileDialog.getSaveFileName(figure, 'Save File')
         if not path:
             return
-        filename = path
+        filename = path + '{}'.format('.py' if '.py' not in path[-3:] else '')
+
     ws = get_workspace_handle(ws_name).raw_ws
     GeneratePythonScript(ws, Filename=filename)
+    add_import_statements(filename)
+
+
+def add_import_statements(filename):
+    with open(filename, 'r+') as generated_script:
+        script_lines = generated_script.readlines()
+        for i, statement in enumerate(import_statements()):
+            script_lines.insert(3 + i, statement)
+        generated_script.seek(0)
+        generated_script.writelines(script_lines)
+
+
+def import_statements():
+    statements = []
+    for package in PACKAGES:
+        statements.append('import {} as {} \n'.format(package, PACKAGES[package]))
+    return statements
+
