Re #454 - fixed 2theta units naming bug

mslice/models/slice/slice_functions.py
@@ -157,7 +157,7 @@ def compute_recoil_line(ws_name, axis, relative_mass=1):
         momentum_transfer = x_axis
         line = np.square(momentum_transfer * 1.e10 * constants.hbar) / (2 * relative_mass * constants.neutron_mass) /\
             (constants.elementary_charge / 1000)
-    elif axis.units == 'Degrees':
+    elif axis.units == 'Degrees' or axis.units == '2Theta':
         tth = x_axis * np.pi / 180.
         if 'Direct' in get_workspace_handle(ws_name).e_mode:
             line = efixed * (2 - 2 * np.cos(tth)) / (relative_mass + 1 - np.cos(tth))
@@ -172,7 +172,7 @@ def compute_powder_line(ws_name, axis, element, cif_file=False):
     efixed = get_workspace_handle(ws_name).e_fixed
     if axis.units == 'MomentumTransfer' or axis.units == '|Q|':
         x0 = _compute_powder_line_momentum(ws_name, axis, element, cif_file)
-    elif axis.units == 'Degrees':
+    elif axis.units == 'Degrees' or axis.units == '2Theta':
         x0 = _compute_powder_line_degrees(ws_name, axis, element, efixed, cif_file)
     else:
         raise RuntimeError("units of axis not recognised")