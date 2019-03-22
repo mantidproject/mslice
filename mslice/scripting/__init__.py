9d867723e957d842330a26f4dd828a104ffc0a5f
Duc Le <duc.le@stfc.ac.uk>
2019-03-22 16:09:27 +0000

    Re #458 - unit tests

--- mslice/scripting/__init__.py
+++ mslice/scripting/__init__.py
@@ -42,22 +42,35 @@ def preprocess_lines(ws_name, plot_handler, ax):
 
 def _get_c_address(mslice_workspace):
     # Hack to get the c-address of the shared pointer to the workspace object (assumes a boost.python.class and CPython)
+    mslice_workspace = get_workspace_handle(mslice_workspace)
     if isinstance(mslice_workspace, WorkspaceBase):
         return ctypes.cast(id(mslice_workspace.raw_ws), ctypes.POINTER(ctypes.c_void_p))[8]
+    return None
+
+def _parse_temp_ws_names(kwargs, current_ws_refs):
+    idx = kwargs.find('__TMP') + 5
+    ws_map = {}
+    while idx > 0:
+        addr_str = kwargs[idx:kwargs.find("'", idx)]
+        ws_map[int(addr_str, 16)] = addr_str
+        idx_tmp = kwargs.find('__TMP', idx)
+        idx =  idx + idx_tmp + 5 if idx_tmp > 0 else idx_tmp
+    for addr, addr_str in list(ws_map.items()):
+        if addr in current_ws_refs.keys():
+            kwargs = kwargs.replace("'__TMP{}'".format(addr_str), current_ws_refs[addr])
+    return kwargs
 
 def generate_script_lines(raw_ws, workspace_name):
     lines = []
     ws_name = workspace_name.replace(".", "_")
     alg_history = raw_ws.getHistory().getAlgorithmHistories()
     existing_ws_refs = []
-    current_ws_refs = {'__TMP{:016x}'.format(_get_c_address(get_workspace_handle(ws))).upper(): ws.replace(".", "_")
-                       for ws in get_visible_workspace_names()}
+    current_ws_refs = {_get_c_address(ws): ws.replace(".", "_") for ws in get_visible_workspace_names()}
     for algorithm in alg_history:
         alg_name = algorithm.name()
-        kwargs, output_ws = get_algorithm_kwargs(algorithm, ws_name, existing_ws_refs)
-        ws_ref = [ws_ref for ws_ref in current_ws_refs.keys() if ws_ref in kwargs]
-        for ref in ws_ref:
-            kwargs = kwargs.replace("'{}'".format(ref), current_ws_refs[ref])
+        kwargs, output_ws = get_algorithm_kwargs(algorithm, existing_ws_refs)
+        if '__TMP' in kwargs:
+            kwargs = _parse_temp_ws_names(kwargs, current_ws_refs)
         if output_ws is not None and output_ws != ws_name:
             lines += ["{} = mc.{}({})\n".format(output_ws, alg_name, kwargs)]
             existing_ws_refs.append(output_ws)
@@ -66,7 +79,7 @@ def generate_script_lines(raw_ws, workspace_name):
     return lines
 
 
-def get_algorithm_kwargs(algorithm, workspace_name, existing_ws_refs):
+def get_algorithm_kwargs(algorithm, existing_ws_refs):
     arguments = []
     output_ws = None
     for prop in algorithm.getProperties():
@@ -82,7 +95,7 @@ def get_algorithm_kwargs(algorithm, workspace_name, existing_ws_refs):
             elif algorithm.name() == "MakeProjection":
                 if prop.name() == "Limits" or prop.name() == "OutputWorkspace" or prop.name() == "ProjectionType":
                     continue
-            if prop.value().replace(".", "_") in existing_ws_refs:
+            if isinstance(prop.value(), str) and prop.value().replace(".", "_") in existing_ws_refs:
                 arguments += ["{}={}".format(prop.name(), prop.value().replace(".", "_"))]
             elif isinstance(prop.value(), str):
                 arguments += ["{}='{}'".format(prop.name(), prop.value())]
