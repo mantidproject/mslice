112be65c4987cb4fcfd5a10bc8c7f4a5fd95c082
Duc Le <duc.le@stfc.ac.uk>
2019-07-03 14:32:41 +0100

    Re #473 - widen `in_mantidplot` check to include workbench

--- mslice/util/mantid/__init__.py
+++ mslice/util/mantid/__init__.py
@@ -1,8 +1,8 @@
 
 def in_mantidplot():
     try:
-        import mantidplot  # noqa: F401
+        from mantidqt.gui_helper import get_qapplication
+        _, is_mantid = get_qapplication()
+        return is_mantid
     except ImportError:
         return False
-    else:
-        return True
