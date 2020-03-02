7a790144c091f2f3ccf1614c782fa3a454a57078
Duc Le <duc.le@stfc.ac.uk>
2020-03-02 09:36:18 +0000

    Re #516 - fixed user defined algorithm import bug

--- mslice/util/mantid/mantid_algorithms.py
+++ mslice/util/mantid/mantid_algorithms.py
@@ -1,10 +1,21 @@
 """Wraps all Mantid algorithms so they use mslice's wrapped workspaces"""
 
 from mantid.simpleapi import * # noqa: F401
+from mantid.simpleapi import _create_algorithm_function
 from mslice.util.mantid.algorithm_wrapper import wrap_algorithm
-from mantid.api import AlgorithmFactory
+from mantid.api import AlgorithmFactory, AlgorithmManager
+from six import iteritems
 
 algorithms = AlgorithmFactory.getRegisteredAlgorithms(False)
 
-for algorithm in algorithms.keys():
-    globals()[algorithm] = wrap_algorithm(globals()[algorithm])
+for algorithm, versions in iteritems(algorithms):
+    try:
+        globals()[algorithm] = wrap_algorithm(globals()[algorithm])
+    except KeyError:   # Possibly a user defined algorithm
+        try:
+            alg_obj = AlgorithmManager.createUnmanaged(algorithm, max(versions))
+            alg_obj.initialize()
+        except Exception:
+            pass
+        else:
+            globals()[algorithm] = wrap_algorithm(_create_algorithm_function(algorithm, max(versions), alg_obj))
