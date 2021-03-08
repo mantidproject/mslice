c691dc27d27a191fbe2ee1cbf3738a1f4cce22d5
Silke Schomann <silke.schomann@stfc.ac.uk>
2021-03-08 15:14:45 +0000

    Added unit test

--- mslice/tests/figure_manager_test.py
+++ mslice/tests/figure_manager_test.py
@@ -131,6 +131,14 @@ class CurrentFigureTest(unittest.TestCase):
         self.assertTrue(fig1 == mock_figures[0])
         self.assertTrue(fig2 == mock_figures[0])
 
+    def test_destroy_all(self, mock_figure_class):
+        mock_figures = [mock.Mock()]
+        mock_figure_class.side_effect = mock_figures
+        fig1 = GlobalFigureManager.get_active_figure()
+        GlobalFigureManager.destroy_all()
+
+        self.assertTrue(fig1 == mock_figures[0])
+
     def test_categorizing_of_uncategorized_plot(self, mock_figure_class):
         mock_figures = [mock.Mock(), mock.Mock(), mock.Mock()]
         fig1_mock_manager = mock.Mock()
