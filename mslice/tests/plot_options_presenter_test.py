d0554065061d1246ab99c45d294f4f78c3a12cb1
Silke Schomann <silke.schomann@stfc.ac.uk>
2021-03-18 16:16:32 +0000

    Removed whitespaces

--- mslice/tests/plot_options_presenter_test.py
+++ mslice/tests/plot_options_presenter_test.py
@@ -212,7 +212,7 @@ class PlotOptionsPresenterTest(unittest.TestCase):
     def test_remove_line(self):
         self.remove_line_by_index = Mock()
         self.presenter = CutPlotOptionsPresenter(self.view, self.model)
-        
+
         # check line with correct index removed
         self.presenter.remove_container(9)
         self.model.remove_line_by_index.assert_called_once_with(9)
