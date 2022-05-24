af133ab3db40c1454fdceae85779e5ca7ba10b60
MialLewis <95620982+MialLewis@users.noreply.github.com>
2022-05-24 17:18:21 +0100

    Fix typo #REF747

--- mslice/plotting/plot_window/plot_options.py
+++ mslice/plotting/plot_window/plot_options.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, division, print_function)
 
-from numpy import arange as np_arrange
+from numpy import arange as np_arange
 from six import iteritems
 import functools
 
@@ -293,7 +293,7 @@ class LegendAndLineOptionsSetter(QtWidgets.QWidget):
         self.width_label = QtWidgets.QLabel(self)
         self.width_label.setText("Width:")
         self.line_width = QtWidgets.QComboBox(self)
-        self.line_width.addItems([str(x) for x in np_arrange(1,10.5,0.5)])
+        self.line_width.addItems([str(x) for x in np_arange(1,10.5,0.5)])
         self.line_width.setCurrentIndex(self.line_width.findText(line_options['width']))
 
         self.marker_label = QtWidgets.QLabel(self)
