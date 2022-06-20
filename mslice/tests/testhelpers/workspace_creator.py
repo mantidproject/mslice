7ff2f5c011d986b0afb059e9bdcdff84659e5aca
Robert Applin <robert.applin@stfc.ac.uk>
2022-06-20 09:03:50 +0100

    Add more tests for cut_algorithm file

--- mslice/tests/testhelpers/workspace_creator.py
+++ mslice/tests/testhelpers/workspace_creator.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-from mantid.simpleapi import AddSampleLog, CreateMDHistoWorkspace, CreateMDWorkspace
+from mantid.simpleapi import (AddSampleLog, CreateMDHistoWorkspace, CreateMDWorkspace, FakeMDEventData)
 
 from mslice.workspace.pixel_workspace import PixelWorkspace
-from mslice.util.mantid.mantid_algorithms import CreateSampleWorkspace, CreateSimulationWorkspace
+from mslice.util.mantid import mantid_algorithms
 
 
 def _create_extents(n_dims: int) -> str:
@@ -29,11 +29,11 @@ def _create_num_of_bins(n_dims: int) -> str:
 def create_simulation_workspace(e_mode: str, output_name: str, psd: bool = False):
     """Creates a basic simulation workspace for testing purposes."""
     if e_mode == "Direct":
-        workspace = CreateSimulationWorkspace(Instrument="MAR", BinParams=[-15, 1, 15], UnitX="DeltaE",
-                                              OutputWorkspace=output_name)
+        workspace = mantid_algorithms.CreateSimulationWorkspace(Instrument="MAR", BinParams=[-15, 1, 15],
+                                                                UnitX="DeltaE", OutputWorkspace=output_name)
     else:
-        workspace = CreateSimulationWorkspace(Instrument="OSIRIS", BinParams=[-15, 1, 15], UnitX="DeltaE",
-                                              OutputWorkspace=output_name)
+        workspace = mantid_algorithms.CreateSimulationWorkspace(Instrument="OSIRIS", BinParams=[-15, 1, 15],
+                                                                UnitX="DeltaE", OutputWorkspace=output_name)
     AddSampleLog(workspace=workspace.raw_ws, LogName='Ei', LogText='3.', LogType='Number', StoreInADS=False)
 
     sim_scattering_data = np.arange(0, 1.5, 0.002).reshape(30, 25).transpose()
@@ -47,8 +47,8 @@ def create_simulation_workspace(e_mode: str, output_name: str, psd: bool = False
 
 def create_workspace(output_name: str):
     """Creates a basic MSlice Workspace for testing purposes."""
-    workspace = CreateSampleWorkspace(NumBanks=1, BankPixelWidth=5, XMin=0.1, XMax=3.1, BinWidth=0.1, XUnit="DeltaE",
-                                      OutputWorkspace=output_name)
+    workspace = mantid_algorithms.CreateSampleWorkspace(NumBanks=1, BankPixelWidth=5, XMin=0.1, XMax=3.1, BinWidth=0.1,
+                                                        XUnit="DeltaE", OutputWorkspace=output_name)
     return workspace
 
 
@@ -65,6 +65,7 @@ def create_md_workspace(n_dims: int, output_name: str):
     """Creates a basic MDWorkspace for testing purposes."""
     md_ws = CreateMDWorkspace(Dimensions=n_dims, Extents=_create_extents(n_dims), Names=_create_dim_names(n_dims),
                               Units=_create_units(n_dims), OutputWorkspace=output_name)
+    FakeMDEventData(InputWorkspace=md_ws, PeakParams=f"500000,{','.join(['0'] * n_dims)},3", RandomizeSignal=True)
     return md_ws
 
 
