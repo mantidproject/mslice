3e31006258462ed0520c1b3c7d79620c52590aa1
MialLewis <95620982+MialLewis@users.noreply.github.com>
2022-07-01 14:43:43 +0100

    revert changes

--- mslice/tests/testhelpers/workspace_creator.py
+++ mslice/tests/testhelpers/workspace_creator.py
@@ -26,17 +26,15 @@ def _create_num_of_bins(n_dims: int) -> str:
     return ",".join(["10"] * n_dims)
 
 
-def create_simulation_workspace(e_mode: str, output_name: str, psd: bool = False, store_in_ads=False):
+def create_simulation_workspace(e_mode: str, output_name: str, psd: bool = False):
     """Creates a basic simulation workspace for testing purposes."""
     if e_mode == "Direct":
         workspace = mantid_algorithms.CreateSimulationWorkspace(Instrument="MAR", BinParams=[-15, 1, 15],
-                                                                UnitX="DeltaE", OutputWorkspace=output_name,
-                                                                StoreInADS=store_in_ads)
+                                                                UnitX="DeltaE", OutputWorkspace=output_name)
     else:
         workspace = mantid_algorithms.CreateSimulationWorkspace(Instrument="OSIRIS", BinParams=[-15, 1, 15],
-                                                                UnitX="DeltaE", OutputWorkspace=output_name,
-                                                                StoreInADS=store_in_ads)
-    AddSampleLog(workspace=workspace.raw_ws, LogName='Ei', LogText='3.', LogType='Number', StoreInADS=False)
+                                                                UnitX="DeltaE", OutputWorkspace=output_name)
+    AddSampleLog(workspace=workspace.raw_ws, LogName='Ei', LogText='3.', LogType='Number')
 
     sim_scattering_data = np.arange(0, 1.5, 0.002).reshape(30, 25).transpose()
     for i in range(25):
@@ -47,36 +45,33 @@ def create_simulation_workspace(e_mode: str, output_name: str, psd: bool = False
     return workspace
 
 
-def create_workspace(output_name: str, store_in_ads=False):
+def create_workspace(output_name: str):
     """Creates a basic MSlice Workspace for testing purposes."""
     workspace = mantid_algorithms.CreateSampleWorkspace(NumBanks=1, BankPixelWidth=5, XMin=0.1, XMax=3.1, BinWidth=0.1,
-                                                        XUnit="DeltaE", OutputWorkspace=output_name,
-                                                        StoreInADS=store_in_ads)
+                                                        XUnit="DeltaE", OutputWorkspace=output_name)
     return workspace
 
 
-def create_md_histo_workspace(n_dims: int, output_name: str, store_in_ads=False):
+def create_md_histo_workspace(n_dims: int, output_name: str):
     """Creates a basic MDHistoWorkspace for testing purposes."""
     md_histo_ws = CreateMDHistoWorkspace(Dimensionality=n_dims, Extents=_create_extents(n_dims),
                                          SignalInput=list(range(0, 100)), ErrorInput=list(range(0, 100)),
                                          NumberOfBins=_create_num_of_bins(n_dims), Names=_create_dim_names(n_dims),
-                                         Units=_create_units(n_dims), OutputWorkspace=output_name,
-                                         StoreInADS=store_in_ads)
+                                         Units=_create_units(n_dims), OutputWorkspace=output_name)
     return md_histo_ws
 
 
-def create_md_workspace(n_dims: int, output_name: str, store_in_ads=False):
+def create_md_workspace(n_dims: int, output_name: str):
     """Creates a basic MDWorkspace for testing purposes."""
     md_ws = CreateMDWorkspace(Dimensions=n_dims, Extents=_create_extents(n_dims), Names=_create_dim_names(n_dims),
-                              Units=_create_units(n_dims), OutputWorkspace=output_name, StoreInADS=store_in_ads)
+                              Units=_create_units(n_dims), OutputWorkspace=output_name)
     FakeMDEventData(InputWorkspace=md_ws, PeakParams=f"500000,{','.join(['0'] * n_dims)},3", RandomizeSignal=True,
                     StoreInADS=store_in_ads)
     return md_ws
 
 
-def create_pixel_workspace(n_dims: int, output_name: str, store_in_ads=False) -> PixelWorkspace:
+def create_pixel_workspace(n_dims: int, output_name: str) -> PixelWorkspace:
     """Creates a basic PixelWorkspace for testing purposes."""
     md_workspace = CreateMDWorkspace(Dimensions=n_dims, Extents=_create_extents(n_dims), Units=_create_units(n_dims),
-                                     Names=_create_dim_names(n_dims), OutputWorkspace=output_name,
-                                     StoreInADS=store_in_ads)
+                                     Names=_create_dim_names(n_dims), OutputWorkspace=output_name)
     return PixelWorkspace(md_workspace, output_name)
