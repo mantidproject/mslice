5af20a0c79ed67b55bc67af338fd8af672c3b9bf
Silke Schomann <silke.schomann@stfc.ac.uk>
2022-10-18 16:24:17 +0100

    Modified linux start scripts

--- mslicedevel.sh
+++ mslicedevel.sh
@@ -3,4 +3,4 @@
 # Basic wrapper script for running in development mode. It assumes the current
 # working directory is the directory containing this script.
 #
-env PYTHONPATH=$(dirname $0):$PYTHONPATH /bin/bash scripts/mslice
+env PYTHONPATH=$(dirname $0):$PYTHONPATH python scripts/start_mslice.py
