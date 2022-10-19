8801e10b61d75827f4ec7c750e6301fff6fc39aa
Silke Schomann <silke.schomann@stfc.ac.uk>
2022-10-19 16:57:11 +0100

    Start conda env automatically

--- mslicedevel.bat
+++ mslicedevel.bat
@@ -2,8 +2,18 @@
 :: Basic wrapper script for running in development mode.
 ::
 @setlocal
+@set ActiveEnv=false
+@if %CONDA_DEFAULT_ENV% == base (
+    @set ActiveEnv=true
+    call conda activate mantidnightly
+)
+@if %CONDA_DEFAULT_ENV% == base (
+    @echo Please run `conda env create -f mslice-developer.yml` first.
+    exit /b 1
+)
 @set THIS_DIR=%~dp0
 @set PYTHONPATH=%~dp0; %CONDA_PREFIX%
 
-python %THIS_DIR%\scripts\start_mslice.py
+python %THIS_DIR%scripts\start_mslice.py
+@if %ActiveEnv%==true call conda deactivate
 @endlocal
