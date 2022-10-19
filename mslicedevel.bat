4fe02eaef574ac2460f2d2a4dcc7e74d90e524be
Silke Schomann <silke.schomann@stfc.ac.uk>
2022-10-19 10:04:08 +0100

    Set PYTHONPATH in batch file only locally

--- mslicedevel.bat
+++ mslicedevel.bat
@@ -1,7 +1,9 @@
 ::
 :: Basic wrapper script for running in development mode.
 ::
+@setlocal
 @set THIS_DIR=%~dp0
-@set PYTHONPATH=%~dp0
+@set PYTHONPATH=%~dp0; %CONDA_PREFIX%
 
-%THIS_DIR%\scripts\start_mslice.py
+python %THIS_DIR%\scripts\start_mslice.py
+@endlocal
