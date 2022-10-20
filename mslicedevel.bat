324cab3404392838cebac38dce2e59581be5648c
Silke Schomann <silke.schomann@stfc.ac.uk>
2022-10-20 15:04:38 +0100

    Mslicedevel.bat with conda and mantidinstall option

--- mslicedevel.bat
+++ mslicedevel.bat
@@ -1,22 +1,59 @@
 ::
 :: Basic wrapper script for running in development mode.
 ::
-
 @setlocal
-@set ActiveEnv=false
-@set CONDAPATH=C:\Users\eoc57742\AppData\Local\mambaforge
-@call %CONDAPATH%\Scripts\activate.bat %CONDAPATH%
-@if %CONDA_DEFAULT_ENV% == base (
-    @set ActiveEnv=true
-    @call %CONDAPATH%\Scripts\activate.bat mantidnightly
-)
-@if %CONDA_DEFAULT_ENV% == base (
-    @echo Please run `conda env create -f mslice-developer.yml` first.
-    exit /b 1
-)
 @set THIS_DIR=%~dp0
-@set PYTHONPATH=%~dp0; %CONDA_PREFIX%
+
+:: Define here the path to your conda installation and uncomment the line
+::@set CONDAPATH=
+
+:: ActiveEnv is set to true once the conda environment mantidnightly was successfully activated
+@set ActiveEnv=false
+
+:: With CONDAPATH set mantidnightly can be activated even when the base environment is not activated yet, for instance by doubleclicking on mslicedevel.bat
+@if defined CONDAPATH (
+    goto CheckBaseEnv
+) else if defined CONDA_DEFAULT_ENV (
+    :: Without CONDAPATH set it is still possible to activate mantidnightly when mslicedevel.bat is run from a conda prompt
+    goto ActivateEnv
+) else (
+    goto NoConda
+)
+
+:CheckBaseEnv
+@if defined CONDA_DEFAULT_ENV (
+    goto ActivateEnv
+) else (
+    :: Activate the base environment first if necessary
+    call %CONDAPATH%\Scripts\activate.bat %CONDAPATH%
+    goto ActivateEnv
+)
+
+:ActivateEnv
+:: Activate mantidnightly
+@if %CONDA_DEFAULT_ENV% == base (
+    call conda activate mantidnightly
+)
+@if %CONDA_DEFAULT_ENV% == mantidnightly (
+    @set ActiveEnv=true
+) else (
+    goto NoConda
+)
+
+:NoConda
+:: This allows to start MSlice when there is no conda available or activating mantidnightly failed
+@set MANTIDPATH=C:\MantidInstall\bin
+@set PYTHONPATH=%MANTIDPATH%;%THIS_DIR%;%PYTHONPATH%
+@set QT_PLUGIN_PATH=%MANTIDPATH%\..\plugins\qt5
+C:\MantidInstall\bin\python.exe %THIS_DIR%\scripts\start_mslice.py
+@goto End
+
+@set PYTHONPATH=%THIS_DIR%; %CONDA_PREFIX%
 
 python %THIS_DIR%scripts\start_mslice.py
-@if %ActiveEnv%==true call conda deactivate
+
+:: Only attempt to deactivate mantidnightly when it was successfully activated
+@if %ActiveEnv% == true call conda deactivate
+
+:End
 @endlocal
