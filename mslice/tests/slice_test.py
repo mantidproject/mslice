22d4b004e57ef3ffe2db26b2f88577049fcc7b55
Silke Schomann <silke.schomann@stfc.ac.uk>
2023-01-11 08:39:36 +0000

    Fixed SliceTest

--- mslice/tests/slice_test.py
+++ mslice/tests/slice_test.py
@@ -53,11 +53,12 @@ class SliceTest(unittest.TestCase):
     def test_d2sigma_computes_if_none(self, compute_d2sigma_fn):
         test_output_workspace = MagicMock()
         test_energy_axis = MagicMock()
+        test_output_workspace.e_fixed = 10
 
         test_slice = self._create_slice(workspace=test_output_workspace, e_axis=test_energy_axis)
         test_slice.d2sigma
         compute_d2sigma_fn.assert_called_once_with(test_output_workspace, test_energy_axis,
-                                                   test_output_workspace.scattering_function.e_fixed)
+                                                   test_output_workspace.e_fixed)
 
     def test_d2sigma_returns_if_already_computed(self):
         test_d2sigma = 5.0
@@ -75,7 +76,7 @@ class SliceTest(unittest.TestCase):
         test_slice = self._create_slice(workspace=test_output_workspace, sample_temp=test_sample_temp,
                                         q_axis=test_momentum_axis, e_axis=test_energy_axis)
         test_slice.symmetrised
-        compute_symmetrised_fn.assert_called_once_with(test_output_workspace, test_sample_temp,test_momentum_axis, test_energy_axis, None)
+        compute_symmetrised_fn.assert_called_once_with(test_output_workspace, test_sample_temp, test_energy_axis, None)
 
     def test_symmetrised_returns_if_already_computed(self):
         test_symmetrised = 5.0
