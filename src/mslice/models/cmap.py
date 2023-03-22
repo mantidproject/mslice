ab7a0c0a18f0a96c66b44230f6a366db84429b3b
Applin <robert.applin@stfc.ac.uk>
2023-03-22 08:47:15 +0000

    Remove old matplotlib version check

--- src/mslice/models/cmap.py
+++ src/mslice/models/cmap.py
@@ -1,10 +1,5 @@
 """
 Colour-map related functionality
 """
-from mslice.util import MPL_COMPAT
-
-ALLOWED_CMAPS = ['jet', 'summer', 'winter', 'coolwarm']
-if not MPL_COMPAT:
-    ALLOWED_CMAPS.insert(0, 'viridis')
-
+ALLOWED_CMAPS = ['viridis', 'jet', 'summer', 'winter', 'coolwarm']
 DEFAULT_CMAP = ALLOWED_CMAPS[0]
