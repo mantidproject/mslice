7f4ed7761adc34ba89fc9f8403d7aa02b059d6ec
Silke Schomann <silke.schomann@stfc.ac.uk>
2024-02-23 15:22:15 +0000

    Add noqa

--- tests/intensity_correction_algs_test.py
+++ tests/intensity_correction_algs_test.py
@@ -3,7 +3,6 @@ from __future__ import (absolute_import, division, print_function)
 from mock import patch, MagicMock, call
 import warnings
 
-import numpy as np
 import unittest
 from copy import copy
 
@@ -15,7 +14,12 @@ from mslice.models.intensity_correction_algs import (compute_boltzmann_dist, com
 from mslice.util.mantid.mantid_algorithms import CreateSampleWorkspace, CreateMDHistoWorkspace
 from mslice.workspace.pixel_workspace import PixelWorkspace
 from mantid.simpleapi import AddSampleLog
+
+
 warnings.filterwarnings('ignore', category=DeprecationWarning)
+
+
+import numpy as np  # noqa: E402
 
 
 def _invert_axes(matrix):
