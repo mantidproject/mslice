0b85206a7b8823062b61b6bc6597c6deed573daa
Silke Schomann <silke.schomann@stfc.ac.uk>
2024-10-09 13:34:06 +0100

    Modify construction of source path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,8 @@ import sys
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../../src'))
+#sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath(os.path.join("../..", "src")))
 
 from mslice import version_info
 
