bdb947a4d8b1b79d9f4ae7355ab968ac91ff40ec
Silke Schomann <silke.schomann@stfc.ac.uk>
2024-10-09 13:55:58 +0100

    Print sys.path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ import sys
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('../../src'))
 sys.path.insert(0, os.path.abspath(os.path.join("../..", "src")))
-
+print(sys.path)
 from mslice import version_info
 
 # -- General configuration ------------------------------------------------
--- src/mslice/__init__.py
+++ src/mslice/__init__.py
@@ -5,7 +5,7 @@ Mantid MSlice
 A PyQt-based version of the MSlice (http://mslice.isis.rl.ac.uk) program based
 on Mantid (http://www.mantidproject.org).
 """
-print("version")
+
 version_info = (2, 9, 1)
 __version__ = '.'.join(map(str, version_info))
 __project_url__ = 'https://github.com/mantidproject/mslice'
