15fca6a5dbcd3e6e90b3d0cc05e17c469f7c23bc
Silke Schomann <silke.schomann@stfc.ac.uk>
2024-10-10 16:19:07 +0100

    Use src/mslice

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ import sys
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath(os.path.join("../..", "src")))
+sys.path.insert(0, os.path.abspath(os.path.join("../..", "src/mslice")))
 
 from mslice import version_info  # noqa: E402
 # version_info = (2, 9, 1)
