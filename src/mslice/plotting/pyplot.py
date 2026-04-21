5c24a7b54bc6b9ada90b971d66781e6973dc487a
Silke Schomann <74243814+SilkeSchomann@users.noreply.github.com>
2026-04-21 11:58:37 +0100

    Updated matplotlib version in toml file to 3.10 (#1169)
    * Updated matplotlib version in toml file to 3.10
    
    * Replaced _docstring.dedent_interpd with _docstring.interpd
    
    * Connect showMessage via set_message

--- src/mslice/plotting/pyplot.py
+++ src/mslice/plotting/pyplot.py
@@ -1165,7 +1165,7 @@ if Figure.legend.__doc__:
 
 ## Axes ##
 
-@_docstring.dedent_interpd
+@_docstring.interpd
 def axes(
     arg: None | tuple[float, float, float, float] = None,
     **kwargs
@@ -1283,7 +1283,7 @@ def cla() -> None:
 
 ## More ways of creating Axes ##
 
-@_docstring.dedent_interpd
+@_docstring.interpd
 def subplot(*args, **kwargs) -> Axes:
     """
     Add an Axes to the current figure or retrieve an existing Axes.
