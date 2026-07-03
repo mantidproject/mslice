735498bab023bf0f66632135d6b5614220d227c4
Tom Hampson <thomas.hampson@stfc.ac.uk>
2026-07-03 18:09:15 +0100

    Use dynamic versioning everywhere based on git tag

--- src/mslice/__init__.py
+++ src/mslice/__init__.py
@@ -6,6 +6,11 @@ A PyQt-based version of the MSlice (http://mslice.isis.rl.ac.uk) program based
 on Mantid (http://www.mantidproject.org).
 """
 
-version_info = (2, 15, 0)
-__version__ = ".".join(map(str, version_info))
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("mslice")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
+
 __project_url__ = "https://github.com/mantidproject/mslice"
