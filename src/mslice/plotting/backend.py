6ccbaa276b9c0376bcc8cc2381cb30405b8622fa
Tom Hampson <thomas.hampson@stfc.ac.uk>
2026-07-07 14:17:04 +0100

    Use qt version agnostic backend to allow Qt6 compatibility

--- src/mslice/plotting/backend.py
+++ src/mslice/plotting/backend.py
@@ -1,9 +1,6 @@
 # system imports
 import importlib
 
-# local imports
-from qtpy import QT_VERSION
-
 
 def get_canvas_and_toolbar_cls():
     """
@@ -22,5 +19,5 @@ def get_backend_module():
     """
     # Pick the relevant QtAgg one for the version we are running
     return importlib.import_module(
-        "matplotlib.backends.backend_qt{}agg".format(QT_VERSION[0])
+        "matplotlib.backends.backend_qtagg"
     )
